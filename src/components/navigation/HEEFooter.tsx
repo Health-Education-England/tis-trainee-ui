c6fcd93f526f4c6d949a92c4273a0285be698069
john12321 <johnomeara72@gmail.com>
2021-11-24 09:35:34 +0000

    feat: Add nav link for contact

--- src/components/navigation/HEEFooter.tsx
+++ src/components/navigation/HEEFooter.tsx
@@ -1,4 +1,5 @@
-import { Footer } from "nhsuk-react-components";
+import { Col, Footer, Row } from "nhsuk-react-components";
+import { NavLink } from "react-router-dom";
 import styles from "./HEEFooter.module.scss";
 
 interface HEEFooterProps {
@@ -10,30 +11,36 @@ const HEEFooter = ({ appVersion }: HEEFooterProps) => {
     <>
       <Footer>
         <Footer.List>
-          <Footer.ListItem
-            className={styles.refLink}
-            data-cy="linkSupport"
-            href="/support"
-          >
-            Contact us
-          </Footer.ListItem>
-          <Footer.ListItem
-            className={styles.refLink}
-            data-cy="linkAbout"
-            href="https://www.hee.nhs.uk"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            About
-          </Footer.ListItem>
-          <Footer.ListItem
-            className={styles.refLink}
-            data-cy="linkPrivacyPolicy"
-            href="https://www.hee.nhs.uk/about/privacy-notice"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Privacy &amp; cookie policy
+          <Footer.ListItem>
+            <Row>
+              <Col width="one-third">
+                <NavLink className={styles.refLink} to={"/support"}>
+                  Contact us
+                </NavLink>
+              </Col>
+              <Col width="one-third">
+                <a
+                  className={styles.refLink}
+                  data-cy="linkAbout"
+                  href="https://www.hee.nhs.uk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  About
+                </a>
+              </Col>
+              <Col width="one-third">
+                <a
+                  className={styles.refLink}
+                  data-cy="linkPrivacyPolicy"
+                  href="https://www.hee.nhs.uk/about/privacy-notice"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Privacy &amp; cookie policy
+                </a>
+              </Col>
+            </Row>
           </Footer.ListItem>
         </Footer.List>
         <Footer.Copyright>&copy; Health Education England</Footer.Copyright>
