c5e43a454c78ce5dc56d3a47074aedadd171951b
Steven Howard <steven.howard@hee.nhs.uk>
2021-12-06 10:25:03 +0000

    feat:set displaying of setupmfa when mfa not set

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { Auth } from "aws-amplify";
+import { CognitoUser } from "amazon-cognito-identity-js";
+import { MFAStatus } from "../../models/MFAStatus";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Profile from "../profile/Profile";
 import FormRPartA from "../forms/formr-part-a/FormRPartA";
@@ -5,20 +9,48 @@ import FormRPartB from "../forms/formr-part-b/FormRPartB";
 import Support from "../support/Support";
 import HowToPrintToPDF from "../forms/HowToPrintToPDF";
 import PageNotFound from "../common/PageNotFound";
+import SetupMFA from "../authentication/mfa/SetupMFA";
 
-export const Main = () => {
+const MainRoutes = (): JSX.Element => {
   return (
-    <main className="nhsuk-width-container nhsuk-u-margin-top-5">
-      <Switch>
-        <Route path="/profile" component={Profile} />
-        <Route path="/formr-a" component={FormRPartA} />
-        <Route path="/formr-b" component={FormRPartB} />
-        <Route path="/support" component={Support} />
-        <Route path="/howtoexport" component={HowToPrintToPDF} />
-        <Redirect exact path="/" to="/profile" />
-
-        <Route path="/*" component={PageNotFound} />
-      </Switch>
-    </main>
+    <Switch>
+      <Route path="/profile" component={Profile} />
+      <Route path="/formr-a" component={FormRPartA} />
+      <Route path="/formr-b" component={FormRPartB} />
+      <Route path="/support" component={Support} />
+      <Route path="/howtoexport" component={HowToPrintToPDF} />
+      <Redirect exact path="/" to="/profile" />
+      <Route path="/*" component={PageNotFound} />
+    </Switch>
+  );
+};
+export const Main = () => {
+  const [user, setUser] = useState<CognitoUser>();
+  const [mfaStatus, setMFAStatus] = useState<string>("");
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const currentUser: CognitoUser = await Auth.currentAuthenticatedUser();
+        const currentMFA: string = await Auth.getPreferredMFA(currentUser);
+        setUser(currentUser);
+        setMFAStatus(currentMFA);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    };
+    getUser();
+  }, []);
+  return (
+    <>
+      {user && (
+        <main className="nhsuk-width-container nhsuk-u-margin-top-5">
+          {mfaStatus === MFAStatus.SMS || mfaStatus === MFAStatus.TOTP ? (
+            <MainRoutes />
+          ) : (
+            <SetupMFA mfaStatus={mfaStatus} user={user} />
+          )}
+        </main>
+      )}
+    </>
   );
 };
