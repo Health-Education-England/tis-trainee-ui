551a2da42b6367f0d423bad99448f00f987a37ae
Steven Howard <steven.howard@hee.nhs.uk>
2021-12-06 10:19:19 +0000

    feat:update scrollto component to support offsetting for sticky header

--- src/components/forms/ScrollTo.tsx
+++ src/components/forms/ScrollTo.tsx
@@ -3,16 +3,36 @@ import React from "react";
 interface IProps {
   location?: "top" | "element";
   scrollType?: "auto" | "smooth";
+  offset?: number;
+  offsetMobile?: number;
 }
 export default class ScrollTo extends React.Component<IProps> {
   elementRef = React.createRef<HTMLDivElement>();
 
   componentDidMount() {
-    if (this.props.location && this.props.location === "element") {
-      if (this.elementRef.current !== null) {
-        this.elementRef.current.scrollIntoView({
-          behavior: this.props.scrollType || "auto"
+    const {
+      location = "top",
+      scrollType,
+      offset,
+      offsetMobile = -40
+    } = this.props;
+    const element = this.elementRef.current;
+
+    if (location === "element") {
+      if (element) {
+        let verticalScroll = element.offsetTop;
+
+        if (window.matchMedia("screen and (max-width: 768px)").matches) {
+          verticalScroll = verticalScroll + offsetMobile;
+        }
+        if (offset) {
+          verticalScroll = verticalScroll + offset;
+        }
+        window.scrollTo({
+          top: verticalScroll,
+          behavior: scrollType || "auto"
         });
+        console.log("SCROLL:", scrollType);
       }
     } else {
       window.scrollTo(0, 0);
