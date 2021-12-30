c8b97b4a6aa2ca1bf4fa5b2b019694df36ed4515
john12321 <johnomeara72@gmail.com>
2021-12-30 09:12:17 +0000

    chore: Add formr-a state to view submitted form

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import traineeProfileReducer from "./traineeProfileSlice";
 import formsReducer from "./formsSlice";
+import formAReducer from "./formASlice";
 
 const rootReducer = combineReducers({
   traineeProfile: traineeProfileReducer,
-  forms: formsReducer
+  forms: formsReducer,
+  formA: formAReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
