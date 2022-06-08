4e77b5f5243289d4d75ccd6a96bb64dc7dcfff8c
Edward Barclay <Edward.Barclay@hee.nhs.uk>
2022-06-08 15:03:28 +0100

    refactor: change date format for localDate

--- src/utilities/DateUtilities.ts
+++ src/utilities/DateUtilities.ts
@@ -23,7 +23,7 @@ export class DateUtilities {
     let localDate = "";
     if (date) {
       const dayDate = day(date);
-      localDate = dayDate.isValid() ? dayDate.format("DD/MM/YYYY") : "";
+      localDate = dayDate.isValid() ? dayDate.format("DD/MM/YYYY HH:mm") : "";
     }
 
     return localDate;
