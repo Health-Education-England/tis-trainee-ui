a7af009d5bd9b532d5edadccf655bb6a3e545c17
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2022-09-12 14:04:23 +0100

    fix: refactor Form R Part B View
    POC: first section refactored.
    
    TIS21-3069

--- src/components/forms/formr-part-b/View.tsx
+++ src/components/forms/formr-part-b/View.tsx
@@ -24,6 +24,9 @@ import { FormRUtilities } from "../../../utilities/FormRUtilities";
 import ScrollTo from "../ScrollTo";
 import classes from "./FormRPartB.module.scss";
 import FormSavePDF from "../FormSavePDF";
+
+import ViewSection1 from "./viewSections/ViewSection1";
+
 interface IView {
   canEdit: boolean;
   history: any;
@@ -56,6 +59,8 @@ const View = ({ canEdit, history }: IView) => {
     );
   };
 
+  const viewSectionProps = { SectionEditButton, formData };
+
   if (formData.traineeTisId)
     content = (
       <>
@@ -72,151 +77,7 @@ const View = ({ canEdit, history }: IView) => {
             </p>
           </WarningCallout>
         )}
-        <div className="nhsuk-grid-row nhsuk-u-margin-top-3">
-          <div className="nhsuk-grid-column-two-thirds">
-            <h2 data-cy="sectionHeader1">Section 1: Doctor's details</h2>
-          </div>
-          <div className="nhsuk-grid-column-one-third">
-            {SectionEditButton(1)}
-          </div>
-        </div>
-        <Card feature>
-          <Card.Content>
-            <Card.Heading>Personal details</Card.Heading>
-            <SummaryList>
-              <SummaryList.Row>
-                <SummaryList.Key>Forename</SummaryList.Key>
-                <SummaryList.Value>{formData.forename}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>GMC-Registered Surname</SummaryList.Key>
-                <SummaryList.Value>{formData.surname}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>GMC Number</SummaryList.Key>
-                <SummaryList.Value data-cy="gmcNumber">
-                  {formData.gmcNumber}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Email Address</SummaryList.Key>
-                <SummaryList.Value>{formData.email}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Deanery / HEE Local Team</SummaryList.Key>
-                <SummaryList.Value data-cy="localOfficeName">
-                  {formData.localOfficeName}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>
-                  Previous Designated Revalidation Body
-                </SummaryList.Key>
-                <SummaryList.Value>{formData.prevRevalBody}</SummaryList.Value>
-              </SummaryList.Row>
-              {formData.prevRevalBodyOther && (
-                <SummaryList.Row>
-                  <SummaryList.Key>
-                    Other Previous Revalidation Body
-                  </SummaryList.Key>
-                  <SummaryList.Value>
-                    {formData.prevRevalBodyOther}
-                  </SummaryList.Value>
-                </SummaryList.Row>
-              )}
-              <SummaryList.Row>
-                <SummaryList.Key>Current Revalidation Date</SummaryList.Key>
-                <SummaryList.Value data-jest="currRevalDate">
-                  {DateUtilities.ToLocalDate(formData.currRevalDate || null)}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Date of Previous Revalidation</SummaryList.Key>
-                <SummaryList.Value data-jest="prevRevalDate">
-                  {DateUtilities.ToLocalDate(formData.prevRevalDate || null)}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>
-                  Programme / Training Specialty
-                </SummaryList.Key>
-                <SummaryList.Value>
-                  {formData.programmeSpecialty}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Dual Specialty</SummaryList.Key>
-                <SummaryList.Value>{formData.dualSpecialty}</SummaryList.Value>
-              </SummaryList.Row>
-            </SummaryList>{" "}
-            <SummaryList>
-              <SummaryList.Row>
-                <SummaryList.Key>Forename</SummaryList.Key>
-                <SummaryList.Value>{formData.forename}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>GMC-Registered Surname</SummaryList.Key>
-                <SummaryList.Value>{formData.surname}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>GMC Number</SummaryList.Key>
-                <SummaryList.Value data-cy="gmcNumber">
-                  {formData.gmcNumber}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Email Address</SummaryList.Key>
-                <SummaryList.Value>{formData.email}</SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Deanery / HEE Local Team</SummaryList.Key>
-                <SummaryList.Value data-cy="localOfficeName">
-                  {formData.localOfficeName}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>
-                  Previous Designated Revalidation Body
-                </SummaryList.Key>
-                <SummaryList.Value>{formData.prevRevalBody}</SummaryList.Value>
-              </SummaryList.Row>
-              {formData.prevRevalBodyOther && (
-                <SummaryList.Row>
-                  <SummaryList.Key>
-                    Other Previous Revalidation Body
-                  </SummaryList.Key>
-                  <SummaryList.Value>
-                    {formData.prevRevalBodyOther}
-                  </SummaryList.Value>
-                </SummaryList.Row>
-              )}
-              <SummaryList.Row>
-                <SummaryList.Key>Current Revalidation Date</SummaryList.Key>
-                <SummaryList.Value data-jest="currRevalDate">
-                  {DateUtilities.ToLocalDate(formData.currRevalDate || null)}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Date of Previous Revalidation</SummaryList.Key>
-                <SummaryList.Value data-jest="prevRevalDate">
-                  {DateUtilities.ToLocalDate(formData.prevRevalDate || null)}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>
-                  Programme / Training Specialty
-                </SummaryList.Key>
-                <SummaryList.Value>
-                  {formData.programmeSpecialty}
-                </SummaryList.Value>
-              </SummaryList.Row>
-              <SummaryList.Row>
-                <SummaryList.Key>Dual Specialty</SummaryList.Key>
-                <SummaryList.Value>{formData.dualSpecialty}</SummaryList.Value>
-              </SummaryList.Row>
-            </SummaryList>
-          </Card.Content>
-        </Card>
+        <ViewSection1 {...viewSectionProps} />
         <div className="nhsuk-grid-row page-break">
           <div className="nhsuk-grid-column-two-thirds">
             <h2 data-cy="sectionHeader2">Section 2: Whole Scope of Practice</h2>
