0caca302688c49cd200e5571db9c79bfb2b093b4
john12321 <johnomeara72@gmail.com>
2022-10-04 11:17:39 +0100

    feat: Add DSP issue btn to Placement panel

--- src/components/profile/placements/PlacementPanel.tsx
+++ src/components/profile/placements/PlacementPanel.tsx
@@ -1,83 +1,132 @@
 import { Placement } from "../../../models/Placement";
-import { SummaryList } from "nhsuk-react-components";
+import { Button, SummaryList } from "nhsuk-react-components";
 import { DateUtilities } from "../../../utilities/DateUtilities";
 import { StringUtilities } from "../../../utilities/StringUtilities";
 
-interface IPlacementPanelProps {
-  placement: Placement;
+interface IDSPSection {
+  children: React.ReactNode;
 }
 
-export const PlacementPanel = ({ placement }: IPlacementPanelProps) => {
+const DSPBtnSection = ({ children }: IDSPSection) => {
   return (
-    <SummaryList>
-      <SummaryList.Row>
-        <SummaryList.Key data-cy="siteKey">Site</SummaryList.Key>
-        <SummaryList.Value data-cy="siteValue">
-          {placement.site}
-        </SummaryList.Value>
-      </SummaryList.Row>
-      <SummaryList.Row>
-        <SummaryList.Key>Site Location</SummaryList.Key>
-        <SummaryList.Value>{placement.siteLocation}</SummaryList.Value>
-      </SummaryList.Row>
+    <div style={{ display: "flex", justifyContent: "center" }}>{children}</div>
+  );
+};
 
-      <SummaryList.Row>
-        <SummaryList.Key>
-          <span className="noWrap">Starts</span>
-        </SummaryList.Key>
-        <SummaryList.Value>
-          {DateUtilities.ToLocalDate(placement.startDate)}
-        </SummaryList.Value>
-      </SummaryList.Row>
-      <SummaryList.Row>
-        <SummaryList.Key>Ends</SummaryList.Key>
-        <SummaryList.Value>
-          {DateUtilities.ToLocalDate(placement.endDate)}
-        </SummaryList.Value>
-      </SummaryList.Row>
-      <SummaryList.Row>
-        <SummaryList.Key>Whole Time Equivalent</SummaryList.Key>
-        <SummaryList.Value data-cy="wteValue">
-          {StringUtilities.TrimZeros(placement.wholeTimeEquivalent)}
-        </SummaryList.Value>
-      </SummaryList.Row>
+interface IDspBtn {
+  placement: Placement;
+  panelKey: number;
+}
 
-      <SummaryList.Row>
-        <SummaryList.Key>
-          <span className="noWrap" data-cy="specialtyKey">
-            Specialty
-          </span>
-        </SummaryList.Key>
-        <SummaryList.Value>{placement.specialty}</SummaryList.Value>
-      </SummaryList.Row>
+const DSPBtn = ({ placement, panelKey }: IDspBtn) => {
+  const cyTag = `btnDSPIssue${panelKey}`;
+  let btnTxt: string = "";
+  let isBtnDisabled: boolean = false;
+  if (DateUtilities.IsPastDate(placement.endDate)) {
+    btnTxt = "Past placements can't be added to your Digital Staff Passport";
+    isBtnDisabled = true;
+  } else
+    btnTxt = "Click here to add this credential to your Digital Staff Passport";
 
-      <SummaryList.Row>
-        <SummaryList.Key>
-          <span className="noWrap" data-cy="grade">
-            Grade
-          </span>
-        </SummaryList.Key>
-        <SummaryList.Value>{placement.grade}</SummaryList.Value>
-      </SummaryList.Row>
+  return (
+    <Button
+      style={{ minWidth: "90%", maxWidth: "90%" }}
+      secondary
+      onClick={(e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        console.dir(placement);
+      }}
+      disabled={isBtnDisabled}
+      data-cy={cyTag}
+    >
+      {btnTxt}
+    </Button>
+  );
+};
+interface IPlacementPanelProps {
+  placement: Placement;
+  panelKey: number;
+}
 
-      <SummaryList.Row>
-        <SummaryList.Key>
-          <span className="noWrap" data-cy="placementType">
-            Placement Type
-          </span>
-        </SummaryList.Key>
-        <SummaryList.Value>{placement.placementType}</SummaryList.Value>
-      </SummaryList.Row>
+export const PlacementPanel = ({
+  placement,
+  panelKey
+}: IPlacementPanelProps) => {
+  return (
+    <>
+      <SummaryList>
+        <SummaryList.Row>
+          <SummaryList.Key data-cy="siteKey">Site</SummaryList.Key>
+          <SummaryList.Value data-cy="siteValue">
+            {placement.site}
+          </SummaryList.Value>
+        </SummaryList.Row>
+        <SummaryList.Row>
+          <SummaryList.Key>Site Location</SummaryList.Key>
+          <SummaryList.Value>{placement.siteLocation}</SummaryList.Value>
+        </SummaryList.Row>
 
-      <SummaryList.Row>
-        <SummaryList.Key data-cy="empBodyKey">Employing Body</SummaryList.Key>
-        <SummaryList.Value>{placement.employingBody}</SummaryList.Value>
-      </SummaryList.Row>
-      <SummaryList.Row>
-        <SummaryList.Key data-cy="traBodyKey">Training Body</SummaryList.Key>
-        <SummaryList.Value>{placement.trainingBody}</SummaryList.Value>
-      </SummaryList.Row>
-    </SummaryList>
+        <SummaryList.Row>
+          <SummaryList.Key>
+            <span className="noWrap">Starts</span>
+          </SummaryList.Key>
+          <SummaryList.Value>
+            {DateUtilities.ToLocalDate(placement.startDate)}
+          </SummaryList.Value>
+        </SummaryList.Row>
+        <SummaryList.Row>
+          <SummaryList.Key>Ends</SummaryList.Key>
+          <SummaryList.Value>
+            {DateUtilities.ToLocalDate(placement.endDate)}
+          </SummaryList.Value>
+        </SummaryList.Row>
+        <SummaryList.Row>
+          <SummaryList.Key>Whole Time Equivalent</SummaryList.Key>
+          <SummaryList.Value data-cy="wteValue">
+            {StringUtilities.TrimZeros(placement.wholeTimeEquivalent)}
+          </SummaryList.Value>
+        </SummaryList.Row>
+
+        <SummaryList.Row>
+          <SummaryList.Key>
+            <span className="noWrap" data-cy="specialtyKey">
+              Specialty
+            </span>
+          </SummaryList.Key>
+          <SummaryList.Value>{placement.specialty}</SummaryList.Value>
+        </SummaryList.Row>
+
+        <SummaryList.Row>
+          <SummaryList.Key>
+            <span className="noWrap" data-cy="grade">
+              Grade
+            </span>
+          </SummaryList.Key>
+          <SummaryList.Value>{placement.grade}</SummaryList.Value>
+        </SummaryList.Row>
+
+        <SummaryList.Row>
+          <SummaryList.Key>
+            <span className="noWrap" data-cy="placementType">
+              Placement Type
+            </span>
+          </SummaryList.Key>
+          <SummaryList.Value>{placement.placementType}</SummaryList.Value>
+        </SummaryList.Row>
+
+        <SummaryList.Row>
+          <SummaryList.Key data-cy="empBodyKey">Employing Body</SummaryList.Key>
+          <SummaryList.Value>{placement.employingBody}</SummaryList.Value>
+        </SummaryList.Row>
+        <SummaryList.Row>
+          <SummaryList.Key data-cy="traBodyKey">Training Body</SummaryList.Key>
+          <SummaryList.Value>{placement.trainingBody}</SummaryList.Value>
+        </SummaryList.Row>
+      </SummaryList>
+      <DSPBtnSection>
+        <DSPBtn placement={placement} panelKey={panelKey} />
+      </DSPBtnSection>
+    </>
   );
 };
 
--- src/components/profile/placements/Placements.tsx
+++ src/components/profile/placements/Placements.tsx
@@ -1,48 +1,34 @@
 import React from "react";
-import { Details, Row, Col } from "nhsuk-react-components";
+import { Details, Card } from "nhsuk-react-components";
 import { PlacementPanel } from "./PlacementPanel";
 import { Placement } from "../../../models/Placement";
-import styles from "./Placements.module.scss";
-
 interface IPlacementProps {
   placements: Placement[];
 }
 
 const Placements: React.FC<IPlacementProps> = ({ placements }) => {
-  const columnWidths: any[] = ["full", "full", "one-half"];
-  let columnWidth = columnWidths[placements?.length]
-    ? columnWidths[placements?.length]
-    : "one-half";
+  const sortedPlacements = [...placements].sort((a: Placement, b: Placement) =>
+    a.startDate > b.startDate ? -1 : b.startDate > a.startDate ? 1 : 0
+  );
 
   return (
     placements && (
       <Details expander data-cy="placementsExpander">
         <Details.Summary>Placements</Details.Summary>
         <Details.Text>
-          <Row className={styles.flexRow}>
-            {placements.length === 0 ? (
-              <div>You are not assigned to any placement</div>
+          <Card.Group>
+            {placements.length > 0 ? (
+              sortedPlacements.map((placement: Placement, index: number) => (
+                <Card.GroupItem key={index} width="one-half">
+                  <Card>
+                    <PlacementPanel panelKey={index} placement={placement} />
+                  </Card>
+                </Card.GroupItem>
+              ))
             ) : (
-              [...placements]
-                .sort((a: Placement, b: Placement) =>
-                  a.startDate > b.startDate
-                    ? -1
-                    : b.startDate > a.startDate
-                    ? 1
-                    : 0
-                )
-                .map(
-                  (
-                    placement: Placement,
-                    index: number | string | undefined
-                  ) => (
-                    <Col key={index} width={columnWidth}>
-                      <PlacementPanel key={index} placement={placement} />
-                    </Col>
-                  )
-                )
+              <div>You are not assigned to any placement</div>
             )}
-          </Row>
+          </Card.Group>
         </Details.Text>
       </Details>
     )
