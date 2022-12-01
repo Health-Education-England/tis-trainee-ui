fcf17b623b356c8820cbf0d1d27410ecca867bf9
john12321 <johnomeara72@gmail.com>
2022-12-01 12:16:27 +0000

    test: Cypress 11 - part 5
    More config mainly for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from "cypress";
+import "@cypress/instrument-cra";
+import cypressOtp from "cypress-otp";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "create-react-app",
-      bundler: "webpack",
+      bundler: "webpack"
     },
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    }
   },
+  e2e: {
+    baseUrl: "http://local.tis-selfservice.com",
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 15000,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json"
+    },
+    setupNodeEvents(on, config) {
+      on("task", { generateOTP: cypressOtp });
+      codeCoverageTask(on, config);
+      return config;
+    }
+  }
 });
