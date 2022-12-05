860411341da2ccf1912524dab5f544baf84bc408
john12321 <johnomeara72@gmail.com>
2022-12-05 10:24:33 +0000

    feat: Make AutocompleteSelect look a bit more NHS

--- src/components/common/AutocompleteSelect.tsx
+++ src/components/common/AutocompleteSelect.tsx
@@ -1,3 +1,4 @@
+import { Label } from "nhsuk-react-components";
 import Select from "react-select";
 
 type AutocompleteSelectProps = {
@@ -13,29 +14,6 @@ type AutocompleteSelectProps = {
   label: string;
 };
 
-// TODO styling WIP
-
-const colourStyles = {
-  option: (baseStyles: any, { isFocused, isSelected }: any) => ({
-    ...baseStyles,
-    background: isFocused ? "#2D8CDC" : isSelected ? "none" : undefined,
-    color: isFocused ? "white" : undefined,
-    zIndex: 1,
-    fontSize: "19px"
-  }),
-  control: (baseStyles: any, { isFocused }: any) => ({
-    ...baseStyles,
-    border: "2px solid #212b32",
-    boxShadow: isFocused ? "inset 0 0 0 2px" : "none",
-    outline: isFocused ? "4px solid #ffeb3b" : "none",
-    outlineOffset: isFocused ? "0" : "0"
-  }),
-  singleValue: (baseStyles: any) => ({
-    ...baseStyles,
-    fontSize: "19px"
-  })
-};
-
 export const AutocompleteSelect: React.FC<AutocompleteSelectProps> = ({
   value,
   onChange,
@@ -48,20 +26,21 @@ export const AutocompleteSelect: React.FC<AutocompleteSelectProps> = ({
     onChange(name, !!val ? val.label : "");
   };
   return (
-    <div style={{ marginBottom: "20px" }}>
-      <label
-        style={{
-          fontWeight: 400,
-          fontSize: "1.1875rem",
-          lineHeight: 1.5,
-          display: "block",
-          marginBottom: 4
-        }}
-        htmlFor="color"
-      >
-        {label}{" "}
-      </label>
+    <div
+      data-cy={name}
+      className={
+        error ? "nhsuk-form-group nhsuk-form-group--error" : "nhsuk-form-group"
+      }
+    >
+      <Label id={`${name}--label`}>{label}</Label>
+      {!!error ? (
+        <span className="nhsuk-error-message">
+          <span className="nhsuk-u-visually-hidden">Error: </span>
+          {error}
+        </span>
+      ) : null}
       <Select
+        aria-labelledby={`${name}--label`}
         options={options}
         onChange={handleChange}
         value={value.label}
@@ -71,11 +50,68 @@ export const AutocompleteSelect: React.FC<AutocompleteSelectProps> = ({
           label: value
         }}
         placeholder="Select or start typing..."
+        className="autocomplete-select"
+        classNamePrefix="react-select"
+        theme={theme => ({
+          ...theme,
+          borderRadius: 0
+        })}
         styles={colourStyles}
       />
-      {!!error ? (
-        <div style={{ color: "red", marginTop: "4px" }}>{error}</div>
-      ) : null}
     </div>
   );
+};
+
+const colourStyles = {
+  option: (baseStyles: any, { isFocused, isSelected }: any) => ({
+    ...baseStyles,
+    background: isFocused ? "#2884FF" : isSelected ? "none" : undefined,
+    color: isFocused ? "white" : undefined,
+    zIndex: 1,
+    fontSize: "1rem",
+    "@media (min-width: 40.0625em)": {
+      ...baseStyles["@media (min-width: 40.0625em)"],
+      fontSize: "1.1875rem"
+    },
+    paddingTop: "1px",
+    paddingBottom: "1px"
+  }),
+  control: (baseStyles: any, { isFocused }: any) => ({
+    ...baseStyles,
+    border: "0.0625rem solid #4C6272",
+    borderColor: "#4C6272",
+    "&:hover": {
+      borderColor: "#4C6272"
+    },
+    boxShadow: isFocused ? "inset 0 0 0 2px" : "none",
+    outline: isFocused ? "4px solid #ffeb3b" : "1px solid #4c6272"
+  }),
+  singleValue: (baseStyles: any) => ({
+    ...baseStyles,
+    fontSize: "1rem",
+    "@media (min-width: 40.0625em)": {
+      ...baseStyles["@media (min-width: 40.0625em)"],
+      fontSize: "1.1875rem"
+    }
+  }),
+  dropdownIndicator: (baseStyles: any) => ({
+    ...baseStyles,
+    padding: "0 2px 0 2px",
+    width: "1.125rem",
+    color: "#212b32"
+  }),
+  clearIndicator: (baseStyles: any) => ({
+    ...baseStyles,
+    padding: "0 2px 0 0",
+    width: "1.125rem",
+    color: "#212b32"
+  }),
+  container: (baseStyles: any) => ({
+    ...baseStyles,
+    maxWidth: "100%",
+    "@media (min-width: 40.0625em)": {
+      ...baseStyles["@media (min-width: 40.0625em)"],
+      maxWidth: "52%"
+    }
+  })
 };
