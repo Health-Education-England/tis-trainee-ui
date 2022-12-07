7cb67be1d916b4fcacf51970d68a6edb63eb3ff9
john12321 <johnomeara72@gmail.com>
2022-12-07 12:18:11 +0000

    fix: flakey tests
    by adding dynamic future dates.

--- src/utilities/__test__/ProfileUtilities.test.ts
+++ src/utilities/__test__/ProfileUtilities.test.ts
@@ -30,7 +30,7 @@ describe("ProfileUtilities", () => {
     const totalLeaveCalc = ProfileUtilities.getTotal(draftFormRPartB);
     expect(totalLeaveCalc).toEqual(totLeave);
   });
-  it("should still return the total leave with null value", () => {
+  it("should still return the total leave with zero initial value", () => {
     const totLeave = draftFormRPartBWithNullCareerBreak.totalLeave;
     const totalLeaveCalc = ProfileUtilities.getTotal(
       draftFormRPartBWithNullCareerBreak
@@ -93,24 +93,20 @@ describe("ProfileUtilities", () => {
   // Trim future work placements
   it("should return a new blank work placement if empty arr", () => {
     const emptyArr = [];
-    const trimmedAndSortedEmptyArr = ProfileUtilities.sortedTrimmedWork(
-      emptyArr
-    );
+    const trimmedAndSortedEmptyArr =
+      ProfileUtilities.sortedTrimmedWork(emptyArr);
     expect(trimmedAndSortedEmptyArr).toEqual([NEW_WORK]);
   });
 
   it("should return the original array of just one placement", () => {
     const oneItemArr = [workArr[0]];
-    const trimmedAndSortedOneItemArr = ProfileUtilities.sortedTrimmedWork(
-      oneItemArr
-    );
+    const trimmedAndSortedOneItemArr =
+      ProfileUtilities.sortedTrimmedWork(oneItemArr);
     expect(trimmedAndSortedOneItemArr).toEqual(oneItemArr);
   });
 
   it("should trim the future placements by start date to one placement (if no two have same start date) and put all placements in desc order by end date", () => {
-    const trimmedAndSorted = ProfileUtilities.sortedTrimmedWork(
-      workArr
-    );
+    const trimmedAndSorted = ProfileUtilities.sortedTrimmedWork(workArr);
     expect(trimmedAndSorted).toEqual(trimmedAndSortedArr);
   });
 
@@ -118,6 +114,8 @@ describe("ProfileUtilities", () => {
     const trimmedAndSorted2 = ProfileUtilities.sortedTrimmedWork(
       workArrWithTwoFutureOnSameDay
     );
-    expect(trimmedAndSorted2).toEqual(trimmedAndSortedWorkArrWithTwoFutureOnSameDay);
+    expect(trimmedAndSorted2).toEqual(
+      trimmedAndSortedWorkArrWithTwoFutureOnSameDay
+    );
   });
 });
