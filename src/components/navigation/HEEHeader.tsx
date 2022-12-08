c45f156d498b7a1b5772173c7bdc2823a984f94e
john12321 <johnomeara72@gmail.com>
2022-12-08 16:44:29 +0000

    feat: make Christmas snow again

--- src/components/navigation/HEEHeader.tsx
+++ src/components/navigation/HEEHeader.tsx
@@ -3,6 +3,8 @@ import { Header } from "nhsuk-react-components";
 import Navbar from "./Navbar";
 import HEEHeaderLogo from "./HEEHeaderLogo";
 import styles from "./HEEHeader.module.scss";
+import SantaSwitch from "react-switch";
+import Snowfall from "react-snowfall";
 interface HEEHeaderProps {
   signOut: any;
   mfa: string;
@@ -14,18 +16,70 @@ const HEEHeader = ({ signOut, mfa }: HEEHeaderProps) => {
     setShowMenu(open);
   };
 
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  const iconStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    fontSize: "18px"
+  };
+
   return (
     <Header className={styles.header}>
       <Header.Container>
         <HEEHeaderLogo />
         <Header.Content>
-          <Header.MenuToggle
-            className={`nhsuk-header__menu-toggle ${
-              showMenu ? "closeMenu" : ""
-            }`}
-            onClick={() => setShowMenu(!showMenu)}
-            data-cy="BtnMenu"
-          />
+          <div className={styles.btnDiv}>
+            <Header.MenuToggle
+              className={`nhsuk-header__menu-toggle ${
+                showMenu ? "closeMenu" : ""
+              }`}
+              onClick={() => setShowMenu(!showMenu)}
+              data-cy="BtnMenu"
+            />
+          </div>
+          <div className={styles.santaSwitchDiv}>
+            <SantaSwitch
+              onChange={handleChange}
+              checked={checked}
+              offColor="#2D8CDC"
+              checkedIcon={
+                <div style={iconStyle}>
+                  <span role="img" aria-label="father Christmas checked icon">
+                    🎅
+                  </span>
+                </div>
+              }
+              uncheckedHandleIcon={
+                <div style={iconStyle}>
+                  <span role="img" aria-label="cry emoji unchecked">
+                    😢
+                  </span>
+                </div>
+              }
+              checkedHandleIcon={
+                <div style={iconStyle}>
+                  <span role="img" aria-label="happy emoji checked">
+                    😁
+                  </span>
+                </div>
+              }
+            />
+          </div>
+          {checked ? (
+            <Snowfall
+              style={{
+                position: "fixed",
+                width: "100vw",
+                height: "100vh"
+              }}
+            />
+          ) : null}
         </Header.Content>
       </Header.Container>
       <Navbar
