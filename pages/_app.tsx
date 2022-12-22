677844030dbe28e145b2016a68e68ec8d67fa73c
john12321 <johnomeara72@gmail.com>
2022-12-22 17:23:59 +0000

    chore: add Hotjar and GTM script tags
    Needs proper testing.
    Also re-added some missing css for main app. Still need to fix Amplify
    styling.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ import { Amplify } from "aws-amplify";
 import config from "../aws-amplify/config";
 // The styles.css import below is needed for Authenticator default theme https://ui.docs.amplify.aws/components/authenticator
 import "@aws-amplify/ui-react/styles.css";
+import Script from "next/script";
+import Head from "next/head";
+
+const GTM_ID: string = "UA-40570867-12";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -32,7 +36,26 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Provider store={store}>
-      {typeof document === "undefined" ? null : <Component {...pageProps} />}
+      {typeof document === "undefined" ? null : (
+        <>
+          <Head>
+            <title>TIS Self-Service</title>
+          </Head>
+          <Script id={"hotjar"} strategy={"lazyOnload"}>
+            {`(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1733748,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}{" "}
+          </Script>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
+          </Script>
+          <Component {...pageProps} />
+        </>
+      )}
     </Provider>
   );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Health Education England TIS Self-Service web application"
+        />
+        <meta name="robots" content="noindex,nofollow" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="stylesheet" media="print" href="/print.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
