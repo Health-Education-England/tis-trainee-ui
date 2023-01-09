db0725d2046e8a5616e1b990a1cbcb8f7eaff55e
john12321 <johnomeara72@gmail.com>
2023-01-09 10:03:31 +0000

    test: Add Cypress e2e tests 1
    Added most of the original tests, fixed some
    errors relating to --isolatedModules error in
    some test files by adding a empty object export at the end of the test file.
    (see Authenticator.spec.ts fo example). Also tidied up the FormRA.spec file where we check the contents of the most recently submitted the form (now the submitted forms are in desc order there's no need to inpercept request etc.).
    TODO: Add FormRB Cypress test.

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+import cypressOtp from "cypress-otp";
+import codeCoverageTask from "@cypress/code-coverage/task";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://local.tis-selfservice.com",
+    specPattern: "cypress/e2e/**/*.spec.{ts,tsx}",
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 15000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on("task", { generateOTP: cypressOtp });
+      codeCoverageTask(on, config);
+      return config;
+    }
+  }
+});
