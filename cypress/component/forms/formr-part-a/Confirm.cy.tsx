9f854b46a8fc58d2a064d6470803d8e8b3d4c684
john12321 <johnomeara72@gmail.com>
2023-01-11 09:21:52 +0000

    test: Add Cypress CT (component tests)
    Reconfigured CT for Next JS.
    TODO: How to integrate code coverage from these tests with Jest (and Cypress e2e?)

---
+++ cypress/component/forms/formr-part-a/Confirm.cy.tsx
@@ -0,0 +1,73 @@
+import { mount } from "cypress/react18";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { submittedFormRPartAs } from "../../../../mock-data/submitted-formr-parta";
+import { useAppDispatch } from "../../../../redux/hooks/hooks";
+import { updatedFormA } from "../../../../redux/slices/formASlice";
+import store from "../../../../redux/store/store";
+import { FormRUtilities } from "../../../../utilities/FormRUtilities";
+import Confirm from "../../../../components/forms/formr-part-a/Confirm";
+import history from "../../../../components/navigation/history";
+import { ConfirmProvider } from "material-ui-confirm";
+import React from "react";
+
+describe("Confirm", () => {
+  it("should not render the page if no tisId", () => {
+    mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <Confirm history={[]} />
+        </Router>
+      </Provider>
+    );
+    cy.get("[data-cy=warningSubmit]").should("not.exist");
+  });
+  it("should mount the Confirm component with declaration and btns", () => {
+    const MockedConfirm = () => {
+      const dispatch = useAppDispatch();
+      dispatch(updatedFormA(submittedFormRPartAs[1]));
+
+      return <Confirm history={[]} />;
+    };
+    mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConfirmProvider>
+            <MockedConfirm />
+          </ConfirmProvider>
+        </Router>
+      </Provider>
+    );
+    cy.get("[data-cy=warningConfirmation]").should(
+      "include.text",
+      "Confirmation"
+    );
+    cy.get("[data-cy=surname]").should("have.text", "Gilliam");
+    cy.contains("Declarations").should("exist");
+    cy.get("[data-cy=declarationTypeVal]").should("include.text", "CESR CP");
+    cy.contains("Important").should("exist");
+    cy.get("[data-cy=warningSubmit] > p").should(
+      "include.text",
+      "By submitting this form, I confirm that the information above is correct"
+    );
+
+    cy.stub(FormRUtilities, "handleSubmitA").as("Submit");
+    cy.get("[data-cy=BtnSubmit]").should("exist").click();
+    cy.get(".MuiDialog-container")
+      .should("exist")
+      .should("include.text", "Please think carefully before submitting");
+    cy.get(".MuiDialogActions-root > :nth-child(2)").click();
+    cy.get("@Submit").should("have.been.called");
+
+    cy.stub(FormRUtilities, "historyPush").as("EditViaLink");
+    cy.get("[data-cy=BtnEdit]").should("exist").click();
+    cy.get("@EditViaLink").should("have.been.called");
+
+    cy.stub(FormRUtilities, "saveDraftA").as("SaveDraft");
+    cy.get("[data-cy=BtnSaveDraft]").should("exist").click();
+    cy.get("@SaveDraft").should("have.been.called");
+
+    cy.get("[data-cy=submissionDateTop]").should("not.exist");
+    cy.get("[data-cy=submissionDate]").should("not.exist");
+  });
+});
---
+++ cypress/component/forms/formr-part-b/Section4.cy.tsx
@@ -0,0 +1,95 @@
+import { mount } from "cypress/react18";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { useAppDispatch } from "../../../../redux/hooks/hooks";
+import { updatedFormB } from "../../../../redux/slices/formBSlice";
+import store from "../../../../redux/store/store";
+import Section4 from "../../../../components/forms/formr-part-b/sections/Section4";
+import { submittedFormRPartBs } from "../../../../mock-data/submitted-formr-partb";
+import { mockedCombinedReference } from "../../../../mock-data/combinedReferenceData";
+import { updatedReference } from "../../../../redux/slices/referenceSlice";
+import history from "../../../../components/navigation/history";
+
+describe("Section4", () => {
+  it("should mount section 4 ", () => {
+    const MockedSection4 = () => {
+      const dispatch = useAppDispatch();
+      dispatch(updatedFormB(submittedFormRPartBs[0]));
+      dispatch(updatedReference(mockedCombinedReference));
+
+      return (
+        <Section4
+          prevSectionLabel="Section 3:\nDeclarations relating to\nGood Medical Practice"
+          nextSectionLabel="Section 5:\nNew Declarations\nsince your last Form R"
+          saveDraft={() => Promise.resolve()}
+          previousSection={null}
+          handleSectionSubmit={() => Promise.resolve()}
+        />
+      );
+    };
+    mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <MockedSection4 />
+        </Router>
+      </Provider>
+    );
+    cy.get("[data-cy=legendFieldset4]")
+      .should("exist")
+      .should("include.text", "Section 4");
+    //click no to check that assosiated panel doen not exist in dom
+    cy.get("[data-cy=havePreviousDeclarations1]").click({ force: true });
+    cy.get('[data-cy="previousDeclarations[0].declarationType"]').should(
+      "not.exist"
+    );
+    //click yes and complete panal
+    cy.get("[data-cy=havePreviousDeclarations0]").click();
+    cy.get('[data-cy="previousDeclarations[0].declarationType"]').should(
+      "exist"
+    );
+    cy.get('[data-cy="previousDeclarations[0].declarationType"]').select(
+      "Complaint"
+    );
+    cy.get('[data-cy="previousDeclarations[0].dateOfEntry"]').click();
+    cy.get('[data-cy="previousDeclarations[0].dateOfEntry"]').type(
+      "2022-05-28"
+    );
+    cy.get('[data-cy="previousDeclarations[0].title"]').type("testTitle");
+    cy.get("#previousDeclarations[0].title--error-message").should("not.exist");
+    cy.get('[data-cy="previousDeclarations[0].locationOfEntry"]').type(
+      "testLocation"
+    );
+    cy.get('[data-cy="btnAddDeclaration"]').click();
+    cy.get('[data-cy="previousDeclarations[1].declarationType"]').should(
+      "exist"
+    );
+    cy.get("[data-cy=closeIcon1] > .nhsuk-icon").click();
+    cy.get(
+      "#declarationPanel1 > :nth-child(1) > .nhsuk-grid-column-one-quarter > h3"
+    ).should("not.exist");
+    //click no on previous unresolved declerations
+    cy.get("[data-cy=havePreviousUnresolvedDeclarations1]").click();
+    cy.get("#previousDeclarationSummary--hint > span").should("not.exist");
+    //click yes on previous unresolved declarations
+    cy.get("[data-cy=havePreviousUnresolvedDeclarations0]").click();
+    cy.get(".nhsuk-form-group > [data-cy=previousDeclarationSummary]").should(
+      "exist"
+    );
+    cy.get(".nhsuk-form-group > [data-cy=previousDeclarationSummary]").type(
+      "test text"
+    );
+    //click yes on click yes on previous unresolved declarations and
+    // no on Previous resolved declarations
+    cy.get("[data-cy=havePreviousDeclarations1]").click();
+    cy.get(
+      "[data-cy=havePreviousUnresolvedDeclarations] > .nhsuk-panel-with-label__label"
+    ).should("not.exist");
+    cy.get(".nhsuk-pagination__page > div")
+      .should("exist")
+      .should("include.text", "Section 5");
+    cy.get("[data-cy=LinkToPreviousSection] > .nhsuk-pagination__page > div")
+      .should("exist")
+      .should("include.text", "Section 3");
+    cy.get("[data-cy=BtnSaveDraft]").should("exist");
+  });
+});
