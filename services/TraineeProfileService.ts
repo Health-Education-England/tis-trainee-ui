44a72bcd621c099a66f06d395f39d7e0de224ed8
Doris Wong <doris.wong@hee.nhs.uk>
2023-04-20 12:01:03 +0100

    feat: endpoint for signing COJ

--- services/TraineeProfileService.ts
+++ services/TraineeProfileService.ts
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios";
 import ApiService from "./apiService";
 import { TraineeProfile } from "../models/TraineeProfile";
+import { ProgrammeMembership } from "../models/ProgrammeMembership";
 
 export class TraineeProfileService extends ApiService {
   constructor() {
@@ -9,5 +10,11 @@ export class TraineeProfileService extends ApiService {
 
   async getTraineeProfile(): Promise<AxiosResponse<TraineeProfile>> {
     return this.get<TraineeProfile>("/profile");
+  }
+
+  async signCoj(
+    programmeMembershipId : string
+  ): Promise<AxiosResponse<ProgrammeMembership>> {
+    return this.post<ProgrammeMembership>(`/programme-membership/${programmeMembershipId}/sign-coj`, null);
   }
 }
