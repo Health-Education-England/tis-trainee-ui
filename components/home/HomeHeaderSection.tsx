b0d556e6b35128d74b797379e9f6f104310694a9
Andy Dingley <Judge40@users.noreply.github.com>
2023-04-25 14:14:22 +0100

    feat: display CoJ alert when ready to sign
    When a CoJ is available to sign, an alert should be shown at the top of
    each page directing the user to sign the Conditions of Joining.
    Once all available CoJ are signed the alert should be removed.
    
    TIS21-4385
    TIS21-4373

--- components/home/HomeHeaderSection.tsx
+++ components/home/HomeHeaderSection.tsx
@@ -1,26 +1,31 @@
+import GlobalAlert from "../main/GlobalAlert";
+
 const HomeHeaderSection = () => {
   return (
-    <section className="nhsuk-hero">
-      <div className="nhsuk-width-container nhsuk-hero--border app-width-container">
-        <div className="nhsuk-hero__wrapper app-hero__wrapper">
-          <h1
-            data-cy="homeWelcomeHeaderText"
-            className="nhsuk-u-margin-bottom-4"
-          >
-            Welcome to TIS Self-Service
-          </h1>
-          <p className="nhsuk-body-l nhsuk-u-margin-bottom-1">
-            Your post-graduate training programme resource
-          </p>
-          <p className="nhsuk-body-m nhsuk-u-margin-bottom-1">
-            Our goal is to improve your training experience by making TIS
-            Self-Service a one-stop-shop to support you through your training
-            programme. We are in the Private Beta delivery phase and plan to add
-            more features in future releases.
-          </p>
+    <>
+      <section className="nhsuk-hero">
+        <div className="nhsuk-width-container nhsuk-hero--border app-width-container">
+          <div className="nhsuk-hero__wrapper app-hero__wrapper">
+            <h1
+              data-cy="homeWelcomeHeaderText"
+              className="nhsuk-u-margin-bottom-4"
+            >
+              Welcome to TIS Self-Service
+            </h1>
+            <p className="nhsuk-body-l nhsuk-u-margin-bottom-1">
+              Your post-graduate training programme resource
+            </p>
+            <p className="nhsuk-body-m nhsuk-u-margin-bottom-1">
+              Our goal is to improve your training experience by making TIS
+              Self-Service a one-stop-shop to support you through your training
+              programme. We are in the Private Beta delivery phase and plan to
+              add more features in future releases.
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <GlobalAlert />
+    </>
   );
 };
 
--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect, Router } from "react-router-dom";
+import { Switch, Route, Redirect, Router, useLocation } from "react-router-dom";
 import Profile from "../profile/Profile";
 import FormRPartA from "../forms/formr-part-a/FormRPartA";
 import FormRPartB from "../forms/formr-part-b/FormRPartB";
@@ -24,6 +24,7 @@ import Placements from "../placements/Placements";
 import Programmes from "../programmes/Programmes";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import Dsp from "../dsp/Dsp";
+import GlobalAlert from "./GlobalAlert";
 
 interface IMain {
   signOut: any;
@@ -73,51 +74,62 @@ export const Main = ({ signOut, appVersion }: IMain) => {
     content = (
       <>
         <Router history={history}>
-          <PageTitle />
-          <HEEHeader signOut={signOut} />
-          <Breadcrumbs />
-          <main className="nhsuk-width-container nhsuk-u-margin-top-5">
-            <Switch>
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/placements" component={Placements} />
-              <Route exact path="/programmes" component={Programmes} />
-              <Route exact path="/profile" component={Profile} />
-              <Route path="/credential" component={Dsp} />
-              <Route path="/formr-a" component={FormRPartA} />
-              <Route path="/formr-b" component={FormRPartB} />
-              <Route exact path="/support" component={Support} />
-              <Route path="/mfa" component={MFA} />
-              <Redirect exact path="/" to="/home" />
-              <Redirect
-                exact
-                path="/credential-issued"
-                to={{
-                  pathname: "/credential/issued",
-                  search: location.search
-                }}
-              />
-              <Redirect
-                exact
-                path="/credential-verified"
-                to={{
-                  pathname: "/credential/issue",
-                  search: location.search
-                }}
-              />
-              <Redirect
-                exact
-                path="/invalid-credential"
-                to={{
-                  pathname: "/credential/invalid",
-                  search: location.search
-                }}
-              />
-              <Route path="/*" component={PageNotFound} />
-            </Switch>
-          </main>
-          <HEEFooter appVersion={appVersion} />
+          <App signOut={signOut} appVersion={appVersion} />
         </Router>
       </>
     );
   return <ConfirmProvider>{content}</ConfirmProvider>;
 };
+
+function App(signOut: any, appVersion: string) {
+  const pathname = useLocation().pathname;
+
+  return (
+    <>
+      <PageTitle />
+      <HEEHeader signOut={signOut} />
+      {pathname !== "/home" ? <GlobalAlert /> : null}
+      <Breadcrumbs />
+      <main className="nhsuk-width-container nhsuk-u-margin-top-5">
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/placements" component={Placements} />
+          <Route exact path="/programmes" component={Programmes} />
+          <Route exact path="/profile" component={Profile} />
+          <Route path="/credential" component={Dsp} />
+          <Route path="/formr-a" component={FormRPartA} />
+          <Route path="/formr-b" component={FormRPartB} />
+          <Route exact path="/support" component={Support} />
+          <Route path="/mfa" component={MFA} />
+          <Redirect exact path="/" to="/home" />
+          <Redirect
+            exact
+            path="/credential-issued"
+            to={{
+              pathname: "/credential/issued",
+              search: location.search
+            }}
+          />
+          <Redirect
+            exact
+            path="/credential-verified"
+            to={{
+              pathname: "/credential/issue",
+              search: location.search
+            }}
+          />
+          <Redirect
+            exact
+            path="/invalid-credential"
+            to={{
+              pathname: "/credential/invalid",
+              search: location.search
+            }}
+          />
+          <Route path="/*" component={PageNotFound} />
+        </Switch>
+      </main>
+      <HEEFooter appVersion={appVersion} />
+    </>
+  );
+}
