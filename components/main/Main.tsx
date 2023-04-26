66f5a44114e4294f94771594fbf1b09f97c4539f
Andy Dingley <Judge40@users.noreply.github.com>
2023-04-26 11:05:41 +0100

    refactor: move router to CRAEntryPoint
    The CRAEntryPoint uses a BrowserRouter with an additional Router in the
    Main component. Refactor to replace the BrowserRouter with Router and
    remove the inner router from Main.
    
    TIS21-4373

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect, Router, useLocation } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import Profile from "../profile/Profile";
 import FormRPartA from "../forms/formr-part-a/FormRPartA";
 import FormRPartB from "../forms/formr-part-b/FormRPartB";
@@ -13,7 +13,6 @@ import { fetchTraineeProfileData } from "../../redux/slices/traineeProfileSlice"
 import { fetchReference } from "../../redux/slices/referenceSlice";
 import Loading from "../common/Loading";
 import MFA from "../authentication/setMfa/MFA";
-import history from "../navigation/history";
 import { ConfirmProvider } from "material-ui-confirm";
 import {
   getCognitoGroups,
@@ -37,6 +36,7 @@ export const Main = ({ signOut, appVersion }: IMain) => {
     state => state.traineeProfile.status
   );
   let content;
+  const pathname = useLocation().pathname;
 
   useEffect(() => {
     dispatch(getPreferredMfa());
@@ -73,63 +73,51 @@ export const Main = ({ signOut, appVersion }: IMain) => {
   )
     content = (
       <>
-        <Router history={history}>
-          <App signOut={signOut} appVersion={appVersion} />
-        </Router>
+        <PageTitle />
+        <HEEHeader signOut={signOut} />
+        {pathname !== "/home" ? <GlobalAlert /> : null}
+        <Breadcrumbs />
+        <main className="nhsuk-width-container nhsuk-u-margin-top-5">
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/placements" component={Placements} />
+            <Route exact path="/programmes" component={Programmes} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/credential" component={Dsp} />
+            <Route path="/formr-a" component={FormRPartA} />
+            <Route path="/formr-b" component={FormRPartB} />
+            <Route exact path="/support" component={Support} />
+            <Route path="/mfa" component={MFA} />
+            <Redirect exact path="/" to="/home" />
+            <Redirect
+              exact
+              path="/credential-issued"
+              to={{
+                pathname: "/credential/issued",
+                search: location.search
+              }}
+            />
+            <Redirect
+              exact
+              path="/credential-verified"
+              to={{
+                pathname: "/credential/issue",
+                search: location.search
+              }}
+            />
+            <Redirect
+              exact
+              path="/invalid-credential"
+              to={{
+                pathname: "/credential/invalid",
+                search: location.search
+              }}
+            />
+            <Route path="/*" component={PageNotFound} />
+          </Switch>
+        </main>
+        <HEEFooter appVersion={appVersion} />
       </>
     );
   return <ConfirmProvider>{content}</ConfirmProvider>;
 };
-
-function App(signOut: any, appVersion: string) {
-  const pathname = useLocation().pathname;
-
-  return (
-    <>
-      <PageTitle />
-      <HEEHeader signOut={signOut} />
-      {pathname !== "/home" ? <GlobalAlert /> : null}
-      <Breadcrumbs />
-      <main className="nhsuk-width-container nhsuk-u-margin-top-5">
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/placements" component={Placements} />
-          <Route exact path="/programmes" component={Programmes} />
-          <Route exact path="/profile" component={Profile} />
-          <Route path="/credential" component={Dsp} />
-          <Route path="/formr-a" component={FormRPartA} />
-          <Route path="/formr-b" component={FormRPartB} />
-          <Route exact path="/support" component={Support} />
-          <Route path="/mfa" component={MFA} />
-          <Redirect exact path="/" to="/home" />
-          <Redirect
-            exact
-            path="/credential-issued"
-            to={{
-              pathname: "/credential/issued",
-              search: location.search
-            }}
-          />
-          <Redirect
-            exact
-            path="/credential-verified"
-            to={{
-              pathname: "/credential/issue",
-              search: location.search
-            }}
-          />
-          <Redirect
-            exact
-            path="/invalid-credential"
-            to={{
-              pathname: "/credential/invalid",
-              search: location.search
-            }}
-          />
-          <Route path="/*" component={PageNotFound} />
-        </Switch>
-      </main>
-      <HEEFooter appVersion={appVersion} />
-    </>
-  );
-}
