9784d230c703fcc13790c17917a0178cbb470496
Doris Wong <doris.wong@hee.nhs.uk>
2023-05-02 15:33:34 +0100

    feat: emphasis COJ agreements
    TIS21-4377

--- components/forms/conditionOfJoining/CojView.tsx
+++ components/forms/conditionOfJoining/CojView.tsx
@@ -1,10 +1,13 @@
 import { Formik } from "formik";
-import { Button } from "nhsuk-react-components";
+import { Button, SummaryList } from "nhsuk-react-components";
+import { useEffect } from "react";
 import { signCoj } from "../../../redux/slices/traineeProfileSlice";
 import store from "../../../redux/store/store";
 import history from "../../navigation/history";
+import MultiChoiceInputField from "../MultiChoiceInputField";
 import ScrollTo from "../ScrollTo";
 import CojGg9 from "./CojGg9";
+import * as Yup from "yup";
 import { Redirect } from "react-router-dom";
 import { updatedsigningCoj } from "../../../redux/slices/userSlice";
 
@@ -27,7 +30,38 @@ function CojDeclarationSection() {
   return (
     <>
       <Formik
-        initialValues={{}}
+        // validateOnChange={false}
+        // validateOnBlur={false}
+        initialValues={{
+          declareProvisional: "",
+          declareSatisfy: "",
+          declareProvide: "",
+          declareInform: "",
+          declareUpToDate: "",
+          declareAttend: "",
+          declareEngage: ""
+        }}
+        validationSchema={Yup.object({
+          declareProvisional: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareSatisfy: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareProvide: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareInform: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareUpToDate: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareAttend: Yup.string().required(
+            "Please confirm your acceptance"
+          ),
+          declareEngage: Yup.string().required("Please confirm your acceptance")
+        })}
         onSubmit={async _values => {
           const signingCojPmId = store.getState().user.signingCojPmId;
           await store.dispatch(signCoj(signingCojPmId));
@@ -35,14 +69,100 @@ function CojDeclarationSection() {
           history.push("/programmes");
         }}
       >
-        {({ handleSubmit, isSubmitting }) => (
+        {({ handleSubmit, isValid, isSubmitting }) => (
           <>
+            <SummaryList noBorder>
+              <SummaryList.Row>
+                <SummaryList.Value>
+                  In addition, I acknowledge the following specific information
+                  requirements:
+                </SummaryList.Value>
+              </SummaryList.Row>
+            </SummaryList>
+            <MultiChoiceInputField
+              id="declareProvisional"
+              type="checkbox"
+              name="declareProvisional"
+              items={[
+                {
+                  label: `I understand that programme and post allocations are provisional and subject to change until confirmed by HEE WM and/or my employing organisation.`,
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareSatisfy"
+              type="checkbox"
+              name="declareSatisfy"
+              items={[
+                {
+                  label:
+                    "I understand that I will need to satisfy all requirements of the programme and curriculum to enable satisfactory sign off, and that this may require a specific time commitment.",
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareProvide"
+              type="checkbox"
+              name="declareProvide"
+              items={[
+                {
+                  label: `I will obtain and provide my School and HEE WM with a professional email address.`,
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareInform"
+              type="checkbox"
+              name="declareInform"
+              items={[
+                {
+                  label: `I will inform my School and HEE WM of any change of my personal contact details and/or personal circumstances that may affect my training programme arrangements.`,
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareUpToDate"
+              type="checkbox"
+              name="declareUpToDate"
+              items={[
+                {
+                  label: `I will keep myself up to date with the latest information available via HEE as well as via the relevant educational and regulatory websites.`,
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareAttend"
+              type="checkbox"
+              name="declareAttend"
+              items={[
+                {
+                  label: `I will attend the minimum number of formal teaching days as required by my School/programme.`,
+                  value: true
+                }
+              ]}
+            />
+            <MultiChoiceInputField
+              id="declareEngage"
+              type="checkbox"
+              name="declareEngage"
+              items={[
+                {
+                  label: `Where applicable, I will fully engage with immigration and employer requirements relating to Tier 2 and Tier 4 UK visas.`,
+                  value: true
+                }
+              ]}
+            />
             <Button
               onClick={(e: { preventDefault: () => void }) => {
                 e.preventDefault();
                 handleSubmit();
               }}
-              disabled={isSubmitting}
+              disabled={!isValid || isSubmitting}
               data-cy="cogSignBtn"
             >
               Click to sign Conditions of Joining agreement
--- cypress/component/programmes/CojView.cy.tsx
+++ cypress/component/programmes/CojView.cy.tsx
@@ -30,6 +30,46 @@ describe("COJ Contents View", () => {
   it("should display COJ contents", () => {
     cy.get("[data-cy=cojHeading]").should("exist");
     cy.contains("General Practice").should("exist");
+    cy.get("[data-cy=declareProvisional0]").should("exist");
     cy.get("[data-cy=cogSignBtn]").should("exist");
+  });
+  it("should show warning and disable button when clicking signCoj button without agreements", () => {
+    cy.get("[data-cy=cogSignBtn]").click();
+    cy.get("#declareProvisional--error-message").should("exist");
+    cy.get("#declareSatisfy--error-message").should("exist");
+    cy.get("#declareProvide--error-message").should("exist");
+    cy.get("#declareInform--error-message").should("exist");
+    cy.get("#declareUpToDate--error-message").should("exist");
+    cy.get("#declareAttend--error-message").should("exist");
+    cy.get("#declareEngage--error-message").should("exist");
+    cy.get("[data-cy=cogSignBtn]").should("be.disabled");
+  });
+  it("should show warnings on unchecked agreements", () => {
+    cy.get("[data-cy=declareProvisional0]").click();
+    cy.get("[data-cy=declareSatisfy0]").click();
+    cy.get("[data-cy=declareProvide0]").click();
+    cy.get("[data-cy=cogSignBtn]").should("be.disabled");
+    cy.get("#declareInform--error-message").should("exist");
+    cy.get("#declareUpToDate--error-message").should("exist");
+    cy.get("#declareAttend--error-message").should("exist");
+    cy.get("#declareEngage--error-message").should("exist");
+  });
+  it("should not show warning and enable button if all agreements are checked", () => {
+    cy.get("[data-cy=declareProvisional0]").click();
+    cy.get("[data-cy=declareSatisfy0]").click();
+    cy.get("[data-cy=declareProvide0]").click();
+    cy.get("[data-cy=declareInform0]").click();
+    cy.get("[data-cy=declareUpToDate0]").click();
+    cy.get("[data-cy=declareAttend0]").click();
+    cy.get("[data-cy=declareEngage0]").click();
+    cy.get("[data-cy=cogSignBtn]").click();
+    cy.get("#declareProvisional--error-message").should("not.exist");
+    cy.get("#declareSatisfy--error-message").should("not.exist");
+    cy.get("#declareProvide--error-message").should("not.exist");
+    cy.get("#declareInform--error-message").should("not.exist");
+    cy.get("#declareUpToDate--error-message").should("not.exist");
+    cy.get("#declareAttend--error-message").should("not.exist");
+    cy.get("#declareEngage--error-message").should("not.exist");
+    cy.get("[data-cy=cogSignBtn]").should("not.be.disabled");
   });
 });
