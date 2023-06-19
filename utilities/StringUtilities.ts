42d50b27ce0ff8c5e9d0a71800fc96516bb97423
john12321 <johnomeara72@gmail.com>
2023-06-19 11:47:23 +0100

    fix: toot validation bug
    so when a user clears a TOOT field, saves draft, reopens form and attempts to navigate to the next section the form field validation will now fire for null (and undefined) field value.

--- utilities/StringUtilities.ts
+++ utilities/StringUtilities.ts
@@ -15,10 +15,15 @@ export class StringUtilities {
     return args.filter((a): a is string => a !== null).join(" ");
   }
 
-  public static validateInteger(val: number) {
-    const strVal: string = val.toString();
-    if (!/^\d+$/.test(strVal)) {
-      return "Whole numbers only. No decimals please";
+  public static validateInteger(val: number | null | undefined) {
+    if (val === null || val === undefined) {
+      return null;
+    } else {
+      const strVal = val.toString();
+      if (!/^\d+$/.test(strVal)) {
+        return "Whole numbers only. No decimals please.";
+      }
+      return null;
     }
   }
 }
