f52c234e2fc7a2b98baa84fc8de19636fc7ebf15
Andy Dingley <Judge40@users.noreply.github.com>
2023-06-29 16:42:55 +0100

    feat: add chatbot
    Add a chatbot using the AWS Lex Web UI solution, the chatbot will be
    loaded in an iframe.
    
    TIS21-4712
    TIS21-4709

---
+++ cypress/e2e/support/Chatbot.spec.ts
@@ -0,0 +1,29 @@
+describe("Chatbot", () => {
+  before(() => {
+    cy.wait(30000);
+    cy.visit("/");
+    // Wait for the chatbot to fully load.
+    cy.wait(5000);
+  });
+
+  it("should display expandable widget", () => {
+    const iframeBody = cy
+      .get("#lex-web-ui-iframe")
+      .should("exist")
+      .find("iframe")
+      .its("0.contentDocument")
+      .should("exist")
+      .its("body")
+      .should("not.be.undefined");
+
+    const lexWeb = iframeBody
+      .find("#lex-web")
+      .should("have.attr", "ui-minimized", "true");
+
+    iframeBody.find("button").click();
+
+    lexWeb.should("not.have.attr", "ui-minimized");
+  });
+});
+
+export {};
