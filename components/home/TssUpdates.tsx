401fdb74d5a6c6b14e7cb9743ec0d3a14fb235a4
john12321 <johnomeara72@gmail.com>
2023-07-21 17:26:19 +0100

    chore: remove redundant whats new code
    Given that we're sticking to the AC's 😁 and only showing the latest 'what's new' post, we can ditch some nasty TS ans CSS.

--- components/home/TssUpdates.tsx
+++ components/home/TssUpdates.tsx
@@ -1,49 +1,16 @@
 import { ListPanel } from "nhsuk-react-components";
 import { useAppSelector } from "../../redux/hooks/hooks";
-import { useMemo } from "react";
 import { Post } from "../../models/WPPost";
 
 type PostSummary = Pick<Post, "id" | "title" | "excerpt">;
 
 export const TssUpdates: React.FC = () => {
   const whatsNewHeader = "What's New";
-  const wpUpdatesUrl =
-    "https://tis-support.hee.nhs.uk/about-tis/welcome-to-the-tss-updates/";
   const whatsNewStatus = useAppSelector(state => state.tssUpdates.status);
-  const whatsNewPosts: Post[] = useAppSelector(
+  const whatsNewPost: Post = useAppSelector(
     state => state.tssUpdates.tssUpdates
-  );
-  const newSummaryPosts = useMemo(() => {
-    const summaryPosts: PostSummary[] = [];
-    if (whatsNewPosts?.length > 0) {
-      for (const post of whatsNewPosts) {
-        summaryPosts.push({
-          id: post.id,
-          title: post.title,
-          excerpt: post.excerpt
-        });
-      }
-    } else return [];
-    return summaryPosts;
-  }, [whatsNewPosts]);
-
-  const addWhatsNewPosts = (): JSX.Element[] | JSX.Element => {
-    if (newSummaryPosts.length === 0) {
-      return <p data-cy="noUpdates">No new updates available at the moment.</p>;
-    }
-    return newSummaryPosts.map(post => {
-      return (
-        <ListPanel.Item key={post.id}>
-          <h3 className="list-panel-header" data-cy={`postTitle${post.id}`}>
-            {post.title.rendered}
-          </h3>
-          <p data-cy={`postExcerpt${post.id}`}>
-            {extractTextFromHTML(post.excerpt.rendered)}
-          </p>
-        </ListPanel.Item>
-      );
-    });
-  };
+  )[0];
+  const { id, title, excerpt }: PostSummary = whatsNewPost ?? {};
 
   if (whatsNewStatus === "loading") {
     return (
@@ -72,23 +39,28 @@ export const TssUpdates: React.FC = () => {
     );
   }
 
+  if (!whatsNewPost) {
+    return (
+      <div className="tss-updates-content" data-cy="tssUpdatesContainer">
+        <WhatsNewHeader />
+        <p data-cy="noUpdates">No new updates available at the moment.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="tss-updates-content" data-cy="tssUpdatesContainer">
-      <h2 data-cy="whatsNewHeader">
-        <AnchorEl
-          clName="nhsuk-link custom-link"
-          hRef={wpUpdatesUrl}
-          text={whatsNewHeader}
-        />
-      </h2>
-      <ListPanel>{addWhatsNewPosts()}</ListPanel>
-      {whatsNewPosts.length > 1 && (
-        <AnchorEl
-          clName="nhsuk-link custom-link"
-          hRef={wpUpdatesUrl}
-          text="Click here to read more"
-        />
-      )}
+      <WhatsNewHeader />
+      <ListPanel>
+        <ListPanel.Item key={id}>
+          <h3 className="list-panel-header" data-cy={`postTitle${id}`}>
+            {title.rendered}
+          </h3>
+          <p data-cy={`postExcerpt${id}`}>
+            {extractTextFromHTML(excerpt.rendered)}
+          </p>
+        </ListPanel.Item>
+      </ListPanel>
     </div>
   );
 };
@@ -103,23 +75,21 @@ function extractTextFromHTML(html: string): string {
   return html;
 }
 
-type AnchorElProps = {
-  clName: string;
-  hRef: string;
-  text: string;
-};
-
-function AnchorEl({ clName, hRef, text }: AnchorElProps): JSX.Element {
+function WhatsNewHeader(): JSX.Element {
+  const wpUpdatesUrl =
+    "https://tis-support.hee.nhs.uk/about-tis/welcome-to-the-tss-updates/";
   return (
-    <a
-      data-cy={`anchorEl_${text}`}
-      title="Click here for more details"
-      className={clName}
-      href={hRef}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {text}
-    </a>
+    <h2 data-cy="whatsNewHeader">
+      <a
+        data-cy="anchorEl_What's New"
+        title="Click here for more details"
+        className="nhsuk-link custom-link"
+        href={wpUpdatesUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        What's New
+      </a>
+    </h2>
   );
 }
