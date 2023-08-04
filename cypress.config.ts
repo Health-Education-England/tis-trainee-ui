e3798def7e9336899005585ac03acef4d654719b
EdwardBarclay <edward.barclay@hee.nhs.uk>
2023-08-04 14:22:44 +0100

    refactore: update references to baseUrl for preProd

--- cypress.config.ts
+++ cypress.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     supportFile: "cypress/support/component.ts"
   },
   e2e: {
-    baseUrl: "https://trainee.tis.nhs.uk/",
+    baseUrl: "https://stage.trainee.tis.nhs.uk/",
     // baseUrl: "http://local.tis-selfservice.com",
     specPattern: "cypress/e2e/**/*.spec.{ts,tsx}",
     chromeWebSecurity: false,
