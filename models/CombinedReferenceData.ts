283d027a83020e43e65f775b097770e405f5822e
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2023-08-22 16:46:21 +0100

    fix: programme membership type dropdown
    Draft forms with non-approved entries will have these hidden (though not
    replaced until the user explicitly selects an item from the approved
    list). This may be acceptable, but should be confirmed.
    
    Tests must be somewhere...
    
    TIS21-4904

--- models/CombinedReferenceData.ts
+++ models/CombinedReferenceData.ts
@@ -12,6 +12,7 @@ export interface CombinedReferenceData {
   immigrationStatus: KeyValue[];
   curriculum: CurriculumKeyValue[];
   declarationType: KeyValue[];
+  programmeMembershipType: KeyValue[];
   covidChangeCircs: KeyValue[];
   [key: string]:
     | KeyValue[]
