7a89c926cf34ce0ece9ceb3a13a1e71a9dfb3df4
john12321 <johnomeara72@gmail.com>
2023-11-03 09:46:29 +0000

    fix: comp tests Plus a bit of 'boy scouting' to make Main test file DRY.

--- cypress/component/main/Main.cy.tsx
+++ cypress/component/main/Main.cy.tsx
@@ -9,89 +9,62 @@ import history from "../../../components/navigation/history";
 import React from "react";
 import { updatedRedirected } from "../../../redux/slices/userSlice";
 import { updatedReferenceStatus } from "../../../redux/slices/referenceSlice";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 describe("Main", () => {
-  const MockedMainLoaded = () => {
-    const dispatch = useAppDispatch();
-    dispatch(updatedTraineeProfileStatus("succeeded"));
-    dispatch(updatedReferenceStatus("succeeded"));
-    dispatch(updatedRedirected(false));
-    return <Main signOut={null} appVersion="1.0.0" />;
+  const mountComponent = (
+    status: string,
+    referenceStatus?: string,
+    redirected?: boolean
+  ) => {
+    const MockedMain: React.FC = () => {
+      const dispatch = useAppDispatch();
+      dispatch(updatedTraineeProfileStatus(status));
+      if (referenceStatus) {
+        dispatch(updatedReferenceStatus(referenceStatus));
+      }
+      if (redirected !== undefined) {
+        dispatch(updatedRedirected(redirected));
+      }
+      return <Main />;
+    };
+
+    mount(
+      <Authenticator.Provider>
+        <Provider store={store}>
+          <Router history={history}>
+            <MockedMain />
+          </Router>
+        </Provider>
+      </Authenticator.Provider>
+    );
   };
 
   it("should return Loading comp if data loading ", () => {
-    const MockedMainLoading = () => {
-      const dispatch = useAppDispatch();
-      dispatch(updatedTraineeProfileStatus("loading"));
-      return <Main signOut={null} appVersion="1.0.0" />;
-    };
-    mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <MockedMainLoading />
-        </Router>
-      </Provider>
-    );
+    mountComponent("loading");
     cy.get("[data-cy=BtnMenu]").should("not.exist");
   });
 
   it("should show bookmark alert when previously redirected", () => {
-    const MockedMainLoaded = () => {
-      const dispatch = useAppDispatch();
-      dispatch(updatedTraineeProfileStatus("succeeded"));
-      dispatch(updatedReferenceStatus("succeeded"));
-      dispatch(updatedRedirected(true));
-      return <Main signOut={null} appVersion="1.0.0" />;
-    };
-
-    mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <MockedMainLoaded />
-        </Router>
-      </Provider>
-    );
+    mountComponent("succeeded", "succeeded", true);
     cy.get("[data-cy=bookmarkAlert]").should("exist");
   });
 
   it("should show bookmark alert when currently redirected", () => {
     history.push("test?redirected=1");
-
-    mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <MockedMainLoaded />
-        </Router>
-      </Provider>
-    );
+    mountComponent("succeeded", "succeeded", false);
     cy.get("[data-cy=bookmarkAlert]").should("exist");
   });
 
   it("should not show bookmark alert when not redirected", () => {
     history.push("test");
-
-    mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <MockedMainLoaded />
-        </Router>
-      </Provider>
-    );
+    mountComponent("succeeded", "succeeded", false);
     cy.get("[data-cy=bookmarkAlert]").should("not.exist");
   });
 
   it("should remove the redirected parameter from the URL", () => {
     history.push("test?redirected=1&abc=123");
-
-    console.log(history);
-    mount(
-      <Provider store={store}>
-        <Router history={history}>
-          <MockedMainLoaded />
-        </Router>
-      </Provider>
-    );
-
+    mountComponent("succeeded", "succeeded", false);
     cy.wait(1000);
     cy.url().then(url => expect(url.endsWith("/test?abc=123")).to.be.true);
   });
