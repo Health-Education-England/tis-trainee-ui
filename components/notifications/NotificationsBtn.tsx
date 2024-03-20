fc71bed23ab839c4d04a4816fbdec849b7489a14
EdwardBarclay <Edward.Barclay@hee.nhs.uk>
2024-03-20 15:12:16 +0000

    feat: add notification btn logic

--- components/notifications/NotificationsBtn.tsx
+++ components/notifications/NotificationsBtn.tsx
@@ -2,30 +2,30 @@ import { Button } from "nhsuk-react-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { Tooltip } from "react-tooltip";
-
+import history from "../navigation/history";
 type NotificationsBtnType = {
   unreadNotificationCount: number;
 };
-
 export const NotificationsBtn = ({
   unreadNotificationCount
 }: NotificationsBtnType) => {
+  const handleBtnClick = () => {
+    history.push("/notifications");
+  };
   return (
     <Button
       type="button"
       className="notification-btn"
       data-cy="signOutBtn"
-      data-tooltip-id="Notifications"
+      data-tooltip-id="NotificationsCount"
+      onClick={handleBtnClick}
     >
       <span>
         <FontAwesomeIcon icon={faBell} size="xs" color="white" />
-        <Tooltip
-          className="tooltip tooltip-row"
-          id="Notifications"
-          place="top"
-          content="Notifications"
-        />
-        {<span className="notification-badge">{unreadNotificationCount}</span>}
+        <Tooltip id="NotificationsCount" content="Read your notifications" />
+        {unreadNotificationCount > 0 && (
+          <span className="notification-badge">{unreadNotificationCount}</span>
+        )}
       </span>
     </Button>
   );
