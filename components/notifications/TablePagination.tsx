002f47e2d2fb17d527305c298b3a70dd66441388
EdwardBarclay <Edward.Barclay@hee.nhs.uk>
2024-03-21 14:58:57 +0000

    refactor: update styling for notification table pagination

--- components/notifications/TablePagination.tsx
+++ components/notifications/TablePagination.tsx
@@ -17,59 +17,63 @@ export function TablePagination({ table }: Readonly<TablePaginationType>) {
   return (
     <Container>
       <Row>
-        <Col width="two-thirds">
-          <button
-            type="button"
-            className="nhsuk-u-margin-right-2 pagination-btn"
-            onClick={() => table.firstPage()}
-            disabled={!table.getCanPreviousPage()}
-            data-cy="NotificationsTableFirstPageBtn"
-          >
-            <FontAwesomeIcon icon={faAngleDoubleLeft} />
-          </button>
-          <button
-            type="button"
-            className="nhsuk-u-margin-right-1 pagination-btn"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-            data-cy="NotificationsTablePreviousPageBtn"
-          >
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </button>
-          <button
-            type="button"
-            className="nhsuk-u-margin-right-2 pagination-btn"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-            data-cy="NotificationsTableNextPageBtn"
-          >
-            <FontAwesomeIcon icon={faAngleRight} />
-          </button>
-          <button
-            type="button"
-            className="nhsuk-u-margin-right-2 pagination-btn"
-            onClick={() => table.lastPage()}
-            disabled={!table.getCanNextPage()}
-            data-cy="NotificationsTableLastPageBtn"
-          >
-            <FontAwesomeIcon icon={faAngleDoubleRight} />
-          </button>
-          <select
-            className="nhsuk-select nhsuk-u-margin-left-1"
-            value={table.getState().pagination.pageSize}
-            onChange={e => {
-              table.setPageSize(Number(e.target.value));
-            }}
-            data-cy="NotificationsTablePageSizeSelect"
-          >
-            {[5, 10, 20, 30].map(pageSize => (
-              <option key={pageSize} value={pageSize}>
-                {`Show ${pageSize} rows`}
-              </option>
-            ))}
-          </select>
+        <Col width="two-thirds" className="nhsuk-mobile-pagination-col">
+          <Row className="nhsuk-mobile-pagination-row">
+            <button
+              type="button"
+              className="nhsuk-u-margin-right-2 pagination-btn"
+              onClick={() => table.firstPage()}
+              disabled={!table.getCanPreviousPage()}
+              data-cy="NotificationsTableFirstPageBtn"
+            >
+              <FontAwesomeIcon icon={faAngleDoubleLeft} />
+            </button>
+            <button
+              type="button"
+              className="nhsuk-u-margin-right-1 pagination-btn"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+              data-cy="NotificationsTablePreviousPageBtn"
+            >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <button
+              type="button"
+              className="nhsuk-u-margin-right-2 pagination-btn"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+              data-cy="NotificationsTableNextPageBtn"
+            >
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <button
+              type="button"
+              className="nhsuk-u-margin-right-2 pagination-btn"
+              onClick={() => table.lastPage()}
+              disabled={!table.getCanNextPage()}
+              data-cy="NotificationsTableLastPageBtn"
+            >
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </button>
+          </Row>
+          <Row className="nhsuk-mobile-pagination-row">
+            <select
+              className="nhsuk-select nhsuk-u-margin-left-1"
+              value={table.getState().pagination.pageSize}
+              onChange={e => {
+                table.setPageSize(Number(e.target.value));
+              }}
+              data-cy="NotificationsTablePageSizeSelect"
+            >
+              {[5, 10, 20, 30].map(pageSize => (
+                <option key={pageSize} value={pageSize}>
+                  {`Show ${pageSize} rows`}
+                </option>
+              ))}
+            </select>
+          </Row>
         </Col>
-        <Col width="one-third">
+        <Col width="one-third" className="nhsuk-mobile-pagination-col">
           {table.getPageCount() >= 2 && (
             <span className="nhsuk-u-font-size-19">
               {`Go to page: `}
