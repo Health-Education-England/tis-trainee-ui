796809fa66a9840591a1b06d2f3ddcb1323a2571
EdwardBarclay <Edward.Barclay@hee.nhs.uk>
2024-03-27 09:50:15 +0000

    test: add e2e test and testing dataset

--- cypress/e2e/notifications/Notifications.spec.ts
+++ cypress/e2e/notifications/Notifications.spec.ts
@@ -1,14 +1,33 @@
 /// <reference types="cypress" />
 /// <reference path="../../support/index.d.ts" />
+import { notificationsMixedTypeData } from "../../../mock-data/notifications";
 
 describe("Notifications", () => {
   beforeEach(() => {
-    cy.signInToTss(30000, "/notifications");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "api/notifications"
+      },
+      {
+        statusCode: 200,
+        body: notificationsMixedTypeData
+      }
+    ).as("getNotifications");
+    cy.signInToTss(300000, "/notifications");
+    cy.wait("@getNotifications");
   });
 
   it("should load the notifications page", () => {
     cy.get('[data-cy="notificationsHeading"]')
       .should("exist")
       .should("contain.text", "Notifications");
+  });
+
+  it("should filter the recived list of notifications so that only IN_APP are visiable", () => {
+    cy.get('[data-cy="NotificationsTablePageSizeSelect"]').select(
+      "Show 30 rows"
+    );
+    cy.get('[data-cy="notificationsTable"] tbody tr').should("have.length", 9);
   });
 });
--- mock-data/notifications.ts
+++ mock-data/notifications.ts
@@ -122,3 +122,138 @@ export const notificationsData: NotificationType[] = [
     statusDetail: null
   }
 ];
+
+export const notificationsMixedTypeData: any[] = [
+  {
+    id: "65f1d6bd3f7898e099514196",
+    tisReference: null,
+    type: "",
+    subject: "WELCOME",
+    subjectText: "empty type",
+    contact: null,
+    sentAt: new Date("2024-03-02T15:21:02.939Z"),
+    readAt: new Date("2024-03-18T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514196",
+    tisReference: null,
+    type: "EMAIL",
+    subject: "WELCOME",
+    subjectText: "Email Type Notification",
+    contact: null,
+    sentAt: new Date("2024-03-02T15:21:02.939Z"),
+    readAt: new Date("2024-03-18T13:41:14.750Z"),
+    status: "Read",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514187",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514188",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "READ",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514189",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514190",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "READ",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514191",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514192",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "READ",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514193",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "SOME TIMES ONLY TIS Self-Service will do",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514194",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "READ",
+    statusDetail: null
+  },
+  {
+    id: "65f1d6bd3f7898e099514195",
+    tisReference: null,
+    type: "IN_APP",
+    subject: "WELCOME",
+    subjectText: "Welcome to TIS Self-Service",
+    contact: null,
+    sentAt: new Date("2024-03-01T15:21:02.939Z"),
+    readAt: new Date("2024-03-19T13:41:14.750Z"),
+    status: "UNREAD",
+    statusDetail: null
+  }
+];
