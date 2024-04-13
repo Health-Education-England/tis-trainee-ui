3982a9709b3f436ce665fbbb2b6fd666d4967c15
john12321 <johnomeara72@gmail.com>
2024-04-14 00:13:38 +0100

    feat: Add basic form-b create files

---
+++ components/forms/form-builder/form-r/part-b/FormRPartB.tsx
@@ -0,0 +1,132 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import ScrollTo from "../../../ScrollTo";
+import PageTitle from "../../../../common/PageTitle";
+import { Fieldset, Label } from "nhsuk-react-components";
+import CreateList from "../../../CreateList";
+import { useAppSelector } from "../../../../../redux/hooks/hooks";
+import PageNotFound from "../../../../common/PageNotFound";
+import formBJson from "./formB.json";
+import FormBuilder, { Form, FormName } from "../../FormBuilder";
+import { getFormBValidationSchema } from "./formBValidationSchema";
+import { useSelectFormData } from "../../../../../utilities/hooks/useSelectFormData";
+import { selectAllReference } from "../../../../../redux/slices/referenceSlice";
+import { DesignatedBodyKeyValue } from "../../../../../models/DesignatedBodyKeyValue";
+import { transformReferenceData } from "../../../../../utilities/FormBuilderUtilities";
+import history from "../../../../navigation/history";
+import { FormView } from "../../FormView";
+import { FormRPartB } from "../../../../../models/FormRPartB";
+import { COVID_RESULT_DECLARATIONS } from "../../../../../utilities/Constants";
+
+export default function FormB() {
+  const preferredMfa = useAppSelector(state => state.user.preferredMfa);
+  const canEditStatus = useAppSelector(state => state.formB.canEdit);
+
+  const redirectPath = "/formr-b";
+  const formJson = formBJson as Form;
+  const formData = useSelectFormData(formJson.name as FormName) as FormRPartB;
+
+  // Note: No point using a JSON feature flag here as we need to check the form data anyways
+  // 1. Determine Covid status
+  const activeCovid =
+    useAppSelector(
+      state => state.featureFlags.featureFlags.formRPartB.covidDeclaration
+    ) || formData.haveCovidDeclarations !== null;
+  const formValidationSchema = getFormBValidationSchema(activeCovid);
+  // 2. Set the json form fields based on the Covid status
+  const finalFormJson = activeCovid
+    ? formJson
+    : {
+        ...formJson,
+        pages: formJson.pages.filter(
+          page => page.pageName !== "COVID 19 self-assessment & declarations"
+        )
+      };
+
+  const referenceData = useAppSelector(selectAllReference);
+  const dbcInternal = [
+    ...referenceData.dbc.filter((db: DesignatedBodyKeyValue) => db.internal),
+    { label: "other", value: "other" }
+  ];
+  const dbcExternal = referenceData.dbc.filter(
+    (db: DesignatedBodyKeyValue) => !db.internal
+  );
+  const yesNo = [
+    { value: "Yes", label: "Yes" },
+    { value: "No", label: "No" }
+  ];
+
+  const covidProgressSelfRate = COVID_RESULT_DECLARATIONS;
+
+  const formOptions = transformReferenceData({
+    ...referenceData,
+    dbcInternal,
+    dbcExternal,
+    yesNo,
+    covidProgressSelfRate
+  });
+  if (preferredMfa === "NOMFA") {
+    return <Redirect to="/mfa" />;
+  }
+  return (
+    <>
+      <PageTitle title="Form R Part-B" />
+      <ScrollTo />
+      <Fieldset>
+        <Fieldset.Legend
+          isPageHeading
+          className="fieldset-legend__header"
+          data-cy="formRBHeading"
+        >
+          Form R (Part B)
+        </Fieldset.Legend>
+        <Label data-cy="formrbLabel" size="s">
+          Trainee registration for Postgraduate Speciality Training
+        </Label>
+      </Fieldset>
+      <Switch>
+        <Route
+          exact
+          path="/formr-b/create"
+          render={() => {
+            return formData.traineeTisId ? (
+              <FormBuilder
+                jsonForm={finalFormJson}
+                fetchedFormData={formData}
+                options={formOptions}
+                validationSchema={formValidationSchema}
+                history={history}
+              />
+            ) : (
+              <Redirect to={redirectPath} />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/formr-b/confirm"
+          render={() => (
+            <FormView
+              canEditStatus={canEditStatus}
+              formJson={finalFormJson}
+              redirectPath={redirectPath}
+              validationSchemaForView={formValidationSchema}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/formr-b/:id"
+          render={() => (
+            <FormView
+              canEditStatus={canEditStatus}
+              formJson={finalFormJson}
+              redirectPath={redirectPath}
+            />
+          )}
+        />
+        <Route exact path="/formr-b" component={CreateList} />
+        <Route path="/formr-b/*" component={PageNotFound} />
+      </Switch>
+    </>
+  );
+}
--- components/forms/form-builder/form-sections/ImportantText.tsx
+++ components/forms/form-builder/form-sections/ImportantText.tsx
@@ -1,8 +1,9 @@
 import { WarningCallout } from "nhsuk-react-components";
+import { Fragment } from "react";
 
-interface ImportantText {
+type ImportantText = {
   txtName: string;
-}
+};
 
 export const ImportantText = ({ txtName }: ImportantText) => {
   return (
@@ -18,25 +19,58 @@ export const ImportantText = ({ txtName }: ImportantText) => {
   );
 };
 
-interface DisplayText {
+const prepopulatedText =
+  "This form has been pre-populated using the information available against your records within the Trainee Information System (TIS). Please check all details and amend where necessary. Amendments made to your details on this form will not update other systems that you may have access to. By submitting this document you are confirming that ALL DETAILS (pre-populated or entered/amended by you) are correct.";
+
+const yourResonsibilityText =
+  "It remains your own responsibility to keep your Designated Body and the GMC informed as soon as possible of any changes to your contact details. Your HEE Local team remains your Designated Body throughout your time in training. You can update your Designated Body on your GMC Online account under 'My Revalidation'.";
+
+const outcome5Text =
+  "Failure to appropriately complete a Form R Part B when requested may result in an Outcome 5 at ARCP (Please refer to latest edition of the COPMed Gold Guide).";
+
+const covid19Text1 =
+  "Please complete this form with the information about your training since your last ARCP review, or this is the first scheduled ARCP in your programme, since the start of your current period of training.";
+const covid19Text2 = "Please comment on:";
+const covid19Text3 =
+  "- Your self-assessment of progress up to the point of COVID 19.";
+const covid19Text4 =
+  "- How your training may have been impacted by COVID 19 e.g. if you have not been able to acquire required competencies/capabilities through lack of appropriate learning opportunities or cancellation of required exams/courses.";
+const covid19Text5 = "- Any other relevant information.";
+const covid19Text6 =
+  "By submitting this document, you are confirming that ALL details are correct and that you have made an honest declaration in accordance with the professional standards set out by the General Medical Council in Good Medical Practice.";
+
+const generateTextElement = (texts: string[]) => (
+  <p>
+    {texts.map(text => (
+      <Fragment key={text}>
+        {text}
+        <br />
+        <br />
+      </Fragment>
+    ))}
+  </p>
+);
+
+type DisplayText = {
   [key: string]: JSX.Element;
-}
+};
 
 const displayText: DisplayText = {
-  FormAImportantNotice: (
-    <p>
-      This form has been pre-populated using the information available against
-      your records within the Trainee Information System (TIS). Please check all
-      details and amend where necessary. Amendments made to your details on this
-      form will not update other systems that you may have access to. By
-      submitting this document you are confirming that ALL DETAILS
-      (pre-populated or entered/amended by you) are correct. <br />
-      <br />
-      It remains your own responsibility to keep your Designated Body and the
-      GMC informed as soon as possible of any changes to your contact details.
-      Your HEE Local team remains your Designated Body throughout your time in
-      training. You can update your Designated Body on your GMC Online account
-      under &#34;My Revalidation&#34;.
-    </p>
-  )
+  FormAImportantNotice: generateTextElement([
+    prepopulatedText,
+    yourResonsibilityText
+  ]),
+  FormBImportantNotice: generateTextElement([
+    prepopulatedText,
+    yourResonsibilityText,
+    outcome5Text
+  ]),
+  Covid19ImportantNotice: generateTextElement([
+    covid19Text1,
+    covid19Text2,
+    covid19Text3,
+    covid19Text4,
+    covid19Text5,
+    covid19Text6
+  ])
 };
