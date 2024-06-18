1619c6a1b5a353bbb213d08dfa33b421e939d324
john12321 <johnomeara72@gmail.com>
2024-06-18 12:24:23 +0100

    fix: Prog e2e
    ...by slapping a conditional on it.

--- cypress/e2e/programmes/Programmes.spec.ts
+++ cypress/e2e/programmes/Programmes.spec.ts
@@ -11,19 +11,24 @@ describe("Programmes", () => {
     cy.get('[data-cy="homeWelcomeHeaderText"]').should("not.exist");
     cy.get(".nhsuk-fieldset__heading").should("contain.text", "Programmes");
 
-    // check cct calc basic functionality
-    cy.get('[data-cy^="cctBtn-"]').first().should("exist").click();
-    cy.get('[data-cy="cct-header"]').should("exist");
-    cy.get('[data-cy="cct-calc-btn"]').should("be.disabled");
-    cy.clickSelect('[data-cy="ftePercents"]');
-    cy.get('[data-cy="cct-pdf-btn"]').should("be.disabled");
-    cy.get('[data-cy="cct-calc-btn"]').click();
-    cy.get('[data-cy="cct-th-wte"]')
-      .should("have.text", "New WTE")
-      .next()
-      .should("have.text", "New End Date");
-    cy.get('[data-cy="cct-pdf-btn"]').should("not.be.disabled");
-    cy.get('[data-cy="cct-close-btn"]').should("exist").click();
+    cy.get("body").then($body => {
+      if ($body.find('[data-cy^="cctBtn-"]').length) {
+        cy.get('[data-cy^="cctBtn-"]').first().should("exist").click();
+        cy.get('[data-cy="cct-header"]').should("exist");
+        cy.get('[data-cy="cct-calc-btn"]').should("be.disabled");
+        cy.clickSelect('[data-cy="ftePercents"]');
+        cy.get('[data-cy="cct-pdf-btn"]').should("be.disabled");
+        cy.get('[data-cy="cct-calc-btn"]').click();
+        cy.get('[data-cy="cct-th-wte"]')
+          .should("have.text", "New WTE")
+          .next()
+          .should("have.text", "New End Date");
+        cy.get('[data-cy="cct-pdf-btn"]').should("not.be.disabled");
+        cy.get('[data-cy="cct-close-btn"]').should("exist").click();
+      } else {
+        cy.get('[data-cy="notAssignedprogrammeMemberships"]').should("exist");
+      }
+    });
   });
 });
 
