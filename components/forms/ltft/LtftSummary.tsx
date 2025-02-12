4cb92954738ee24bad1fbc51dfd332d907e87e37
Doris Wong <doris.wong@hee.nhs.uk>
2025-02-12 14:21:17 +0000

    fix: remove ltftListStatus from ltft summary temporarily

--- components/forms/ltft/LtftSummary.tsx
+++ components/forms/ltft/LtftSummary.tsx
@@ -27,7 +27,6 @@ const LtftSummary = ({ ltftSummaryList }: Readonly<LtftSummaryProps>) => {
   //   state => state.ltftSummaryList?.ltftList || []
   // );
   // const ltftListStatus = useAppSelector(state => state.ltftSummaryList?.status);
-  const ltftListStatus = "succeeded";
   const ltftSummaries = mockLtftsList1 || [];
 
   const [showSubmitted, setShowSubmitted] = useState(true);
@@ -55,100 +54,98 @@ const LtftSummary = ({ ltftSummaryList }: Readonly<LtftSummaryProps>) => {
 
   let content: JSX.Element = <></>;
   // if (ltftListStatus === "loading") content = <Loading />;
-  if (ltftListStatus === "succeeded")
-    content = (
-      <div>
-        <CheckboxField
-          data-cy="filterApprovedLtft"
-          name="yesToShowApproved"
-          value="yes"
-          label="APPROVED"
-          checked={showApproved}
-          onChange={() =>
-            setShowApproved(prevShowApproved => !prevShowApproved)
-          }
-        />
-        <CheckboxField
-          data-cy="filterSubmittedLtft"
-          name="yesToShowSubmitted"
-          value="yes"
-          label="SUBMITTED"
-          checked={showSubmitted}
-          onChange={() =>
-            setShowSubmitted(prevShowSubmitted => !prevShowSubmitted)
-          }
-        />
-        <CheckboxField
-          data-cy="filterWithdrawnLtft"
-          name="yesToShowWithdrawn"
-          value="yes"
-          label="WITHDRAWN"
-          checked={showWithdrawn}
-          onChange={() =>
-            setShowWithdrawn(prevShowWithdrawn => !prevShowWithdrawn)
-          }
-        />
-        <Table responsive data-cy="ltftSummary">
-          <Table.Head>
-            <Table.Row>
-              <Table.Cell>Name</Table.Cell>
-              <Table.Cell>Created date</Table.Cell>
-              <Table.Cell>Status</Table.Cell>
-              <Table.Cell>Status date</Table.Cell>
-              <Table.Cell>Operations</Table.Cell>
-            </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            {sortedLtftSummaries.map((item, index) => {
-              return (
-                <Table.Row
-                  key={index}
-                  // TODO: click to show LTFT details
-                  // onClick={e => {
-                  //   e.stopPropagation();
-                  //   history.push(`/ltft/view/${row.original.id}`);
-                  // }}
-                >
-                  <Table.Cell>{item.name}</Table.Cell>
-                  <Table.Cell>
-                    {new Date(item.created).toLocaleDateString()}
-                  </Table.Cell>
-                  <Table.Cell>{item.status}</Table.Cell>
-                  <Table.Cell data-cy={`lastModified-${index}`}>
-                    {new Date(item.lastModified).toLocaleDateString()}
-                  </Table.Cell>
-                  <Table.Cell>
-                    {item.status === "SUBMITTED" && item === latestSubmitted ? (
-                      <>
-                        <Button
-                          data-cy="unsubmitLtftBtnLink"
-                          fontWeight="normal"
-                          // onClick={onClickEvent}
-                          size="small"
-                          type="reset"
-                          style={{ marginRight: "0.5em" }}
-                        >
-                          Unsubmit
-                        </Button>
-                        <Button
-                          data-cy="withdrawLtftBtnLink"
-                          fontWeight="normal"
-                          // onClick={onClickEvent}
-                          size="small"
-                          type="reset"
-                        >
-                          Withdraw
-                        </Button>
-                      </>
-                    ) : null}
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-      </div>
-    );
+  // if (ltftListStatus === "succeeded")
+  content = (
+    <div>
+      <CheckboxField
+        data-cy="filterApprovedLtft"
+        name="yesToShowApproved"
+        value="yes"
+        label="APPROVED"
+        checked={showApproved}
+        onChange={() => setShowApproved(prevShowApproved => !prevShowApproved)}
+      />
+      <CheckboxField
+        data-cy="filterSubmittedLtft"
+        name="yesToShowSubmitted"
+        value="yes"
+        label="SUBMITTED"
+        checked={showSubmitted}
+        onChange={() =>
+          setShowSubmitted(prevShowSubmitted => !prevShowSubmitted)
+        }
+      />
+      <CheckboxField
+        data-cy="filterWithdrawnLtft"
+        name="yesToShowWithdrawn"
+        value="yes"
+        label="WITHDRAWN"
+        checked={showWithdrawn}
+        onChange={() =>
+          setShowWithdrawn(prevShowWithdrawn => !prevShowWithdrawn)
+        }
+      />
+      <Table responsive data-cy="ltftSummary">
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell>Name</Table.Cell>
+            <Table.Cell>Created date</Table.Cell>
+            <Table.Cell>Status</Table.Cell>
+            <Table.Cell>Status date</Table.Cell>
+            <Table.Cell>Operations</Table.Cell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {sortedLtftSummaries.map((item, index) => {
+            return (
+              <Table.Row
+                key={index}
+                // TODO: click to show LTFT details
+                // onClick={e => {
+                //   e.stopPropagation();
+                //   history.push(`/ltft/view/${row.original.id}`);
+                // }}
+              >
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>
+                  {new Date(item.created).toLocaleDateString()}
+                </Table.Cell>
+                <Table.Cell>{item.status}</Table.Cell>
+                <Table.Cell data-cy={`lastModified-${index}`}>
+                  {new Date(item.lastModified).toLocaleDateString()}
+                </Table.Cell>
+                <Table.Cell>
+                  {item.status === "SUBMITTED" && item === latestSubmitted ? (
+                    <>
+                      <Button
+                        data-cy="unsubmitLtftBtnLink"
+                        fontWeight="normal"
+                        // onClick={onClickEvent}
+                        size="small"
+                        type="reset"
+                        style={{ marginRight: "0.5em" }}
+                      >
+                        Unsubmit
+                      </Button>
+                      <Button
+                        data-cy="withdrawLtftBtnLink"
+                        fontWeight="normal"
+                        // onClick={onClickEvent}
+                        size="small"
+                        type="reset"
+                      >
+                        Withdraw
+                      </Button>
+                    </>
+                  ) : null}
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  );
   return content;
 };
 
