4e69bae291e4d447de7e381aed1e819545763d32
john12321 <johnomeara72@gmail.com>
2025-02-28 12:56:34 +0000

    test: new StartOverButton logic

--- components/forms/__test__/StartOverButton.test.tsx
+++ components/forms/__test__/StartOverButton.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 import store from "../../../redux/store/store";
 import { StartOverButton } from "../StartOverButton";
 import { Provider } from "react-redux";
@@ -16,6 +16,7 @@ import { LifeCycleState } from "../../../models/LifeCycleState";
 import { DraftFormProps } from "../../../utilities/FormBuilderUtilities";
 import { useLocation } from "react-router-dom";
 import history from "../../navigation/history";
+import { updatedLtftFormsRefreshNeeded } from "../../../redux/slices/ltftSummaryListSlice";
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
@@ -34,7 +35,10 @@ jest.mock("material-ui-confirm", () => ({
 }));
 
 jest.mock("../../../utilities/FormBuilderUtilities", () => ({
-  isFormDeleted: jest.fn()
+  getDraftFormId: jest.fn(),
+  isFormDeleted: jest.fn(),
+  mapFormNameToUrl: jest.fn(),
+  resetForm: jest.fn()
 }));
 
 jest.mock("../../navigation/history", () => ({
@@ -42,19 +46,28 @@ jest.mock("../../navigation/history", () => ({
 }));
 
 describe("StartOverButton Component", () => {
-  const renderStartOverButton = () => {
+  const renderStartOverButton = (props = {}) => {
     return render(
       <Provider store={store}>
-        <StartOverButton formName="formA" btnLocation="form" />
+        <StartOverButton formName="formA" btnLocation="form" {...props} />
       </Provider>
     );
   };
 
   beforeEach(() => {
     (useLocation as jest.Mock).mockReturnValue({ pathname: "/formr-a" });
+    require("../../../utilities/FormBuilderUtilities").getDraftFormId.mockReturnValue(
+      "123"
+    );
+    require("../../../utilities/FormBuilderUtilities").mapFormNameToUrl.mockReturnValue(
+      "formr-a"
+    );
   });
 
   it("should NOT render the StartOver button when NO formId is available and NO autosave success", () => {
+    require("../../../utilities/FormBuilderUtilities").getDraftFormId.mockReturnValue(
+      null
+    );
     const { queryByRole } = renderStartOverButton();
     const startOverButton = queryByRole("button", {
       name: "Start over"
@@ -72,7 +85,7 @@ describe("StartOverButton Component", () => {
     expect(startOverButton).toBeInTheDocument();
   });
 
-  it("should render the StartOver button when formId is available (i.e. from fetched draft form)", () => {
+  it("should render the StartOver button when formId is available (i.e. from fetched draft form)", async () => {
     const draftProps: DraftFormProps = {
       id: "123",
       lifecycleState: LifeCycleState.Draft
@@ -86,22 +99,77 @@ describe("StartOverButton Component", () => {
     expect(startOverButton).toBeInTheDocument();
   });
 
-  it("should call history.push when successful form delete triggered from 'start over' click on forms page (pathname ends with 'create')", async () => {
-    (useLocation as jest.Mock).mockReturnValue({ pathname: "/formr-a/create" });
-    const mockIsFormDeleted = jest.fn().mockResolvedValue(true);
-    require("../../../utilities/FormBuilderUtilities").isFormDeleted.mockImplementationOnce(
-      mockIsFormDeleted
+  it("should render the StartOver button when btnLocation is 'formView' and NO formId is available", async () => {
+    require("../../../utilities/FormBuilderUtilities").getDraftFormId.mockReturnValue(
+      null
+    );
+    const { queryByRole } = renderStartOverButton({ btnLocation: "formView" });
+    const startOverButton = queryByRole("button", {
+      name: "Start over"
+    });
+    expect(startOverButton).toBeInTheDocument();
+
+    await act(async () => {
+      fireEvent.click(startOverButton as HTMLElement);
+    });
+
+    expect(
+      require("../../../utilities/FormBuilderUtilities").isFormDeleted
+    ).not.toHaveBeenCalled();
+    expect(history.push).toHaveBeenCalledWith("/formr-a");
+  });
+
+  it("should call isFormDeleted and checkPush when formId is available and form is deleted", async () => {
+    require("../../../utilities/FormBuilderUtilities").isFormDeleted.mockResolvedValue(
+      true
     );
     const { queryByRole } = renderStartOverButton();
     const startOverButton = queryByRole("button", {
       name: "Start over"
     });
+    expect(startOverButton).toBeInTheDocument();
 
     await act(async () => {
       fireEvent.click(startOverButton as HTMLElement);
     });
-    expect(mockIsFormDeleted).toHaveBeenCalled();
+
+    expect(
+      require("../../../utilities/FormBuilderUtilities").isFormDeleted
+    ).toHaveBeenCalledWith("formA", "123");
     expect(history.push).toHaveBeenCalledWith("/formr-a");
+  });
+
+  it("should log 'startover failed' when formId is available and form is not deleted", async () => {
+    console.log = jest.fn();
+    require("../../../utilities/FormBuilderUtilities").isFormDeleted.mockResolvedValue(
+      false
+    );
+    const { queryByRole } = renderStartOverButton();
+    const startOverButton = queryByRole("button", {
+      name: "Start over"
+    });
+    expect(startOverButton).toBeInTheDocument();
+
+    await act(async () => {
+      fireEvent.click(startOverButton as HTMLElement);
+    });
+
+    expect(
+      require("../../../utilities/FormBuilderUtilities").isFormDeleted
+    ).toHaveBeenCalledWith("formA", "123");
+    expect(console.log).toHaveBeenCalledWith("startover failed");
+  });
+
+  it("should call resetForm with the correct formName", () => {
+    const { queryByRole } = renderStartOverButton();
+    const startOverButton = queryByRole("button", {
+      name: "Start over"
+    });
+    fireEvent.click(startOverButton as HTMLElement);
+
+    expect(
+      require("../../../utilities/FormBuilderUtilities").resetForm
+    ).toHaveBeenCalledWith("formA");
   });
 
   it("should call redux dispatch (updatedFormsRefreshNeeded) when successful form delete triggered from 'start over' click on forms list page (CreateList comp)", async () => {
@@ -110,7 +178,10 @@ describe("StartOverButton Component", () => {
       mockIsFormDeleted
     );
     const dispatchSpy = jest.spyOn(store, "dispatch");
-    const { queryByRole } = renderStartOverButton();
+    const { queryByRole } = renderStartOverButton({
+      btnLocation: "formsList",
+      formsListDraftId: "123"
+    });
     const startOverButton = queryByRole("button", {
       name: "Start over"
     });
@@ -121,21 +192,54 @@ describe("StartOverButton Component", () => {
     expect(mockIsFormDeleted).toHaveBeenCalled();
     expect(dispatchSpy).toHaveBeenCalledWith(updatedFormsRefreshNeeded(true));
     mockIsFormDeleted.mockReset();
+    dispatchSpy.mockRestore();
   });
 
-  it('should console.log("startover failed") for an unsuccessful form delete (when isFormDeleted returns false)', async () => {
-    const mockIsFormDeleted = jest.fn().mockResolvedValue(false);
+  it("should call redux dispatch (updatedLtftFormsRefreshNeeded) when btnLocation is 'formsList' and formName is 'ltft'", async () => {
+    const mockIsFormDeleted = jest.fn().mockResolvedValue(true);
     require("../../../utilities/FormBuilderUtilities").isFormDeleted.mockImplementationOnce(
       mockIsFormDeleted
     );
-    const consoleSpy = jest.spyOn(console, "log");
-    const { queryByRole } = renderStartOverButton();
+    const dispatchSpy = jest.spyOn(store, "dispatch");
+    const { queryByRole } = renderStartOverButton({
+      formName: "ltft",
+      btnLocation: "formsList",
+      formsListDraftId: "123"
+    });
     const startOverButton = queryByRole("button", {
       name: "Start over"
     });
+
     await act(async () => {
       fireEvent.click(startOverButton as HTMLElement);
     });
-    expect(consoleSpy).toHaveBeenCalledWith("startover failed");
+    expect(mockIsFormDeleted).toHaveBeenCalled();
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      updatedLtftFormsRefreshNeeded(true)
+    );
+    mockIsFormDeleted.mockReset();
+    dispatchSpy.mockRestore();
+  });
+
+  it("should navigate to the correct URL when btnLocation is not 'formsList'", async () => {
+    const mockIsFormDeleted = jest.fn().mockResolvedValue(true);
+    require("../../../utilities/FormBuilderUtilities").isFormDeleted.mockImplementationOnce(
+      mockIsFormDeleted
+    );
+    const { queryByRole } = renderStartOverButton({
+      formName: "formA",
+      btnLocation: "formView",
+      formsListDraftId: "123"
+    });
+    const startOverButton = queryByRole("button", {
+      name: "Start over"
+    });
+
+    await act(async () => {
+      fireEvent.click(startOverButton as HTMLElement);
+    });
+    expect(mockIsFormDeleted).toHaveBeenCalled();
+    expect(history.push).toHaveBeenCalledWith("/formr-a");
+    mockIsFormDeleted.mockReset();
   });
 });
