cb17d6686c5ec5129bd9b30513ae609681d057a0
john12321 <johnomeara72@gmail.com>
2025-05-18 20:34:52 +0100

    feat: Add modal to CCT delete
    ... to confirm deletion. Reuse the ActionModal component for this.
    refactor: table buttons to use the built-in Amplify button styles.

--- components/forms/cct/CctSavedDraftsTable.tsx
+++ components/forms/cct/CctSavedDraftsTable.tsx
@@ -29,6 +29,7 @@ import { populateLtftDraft } from "../../../utilities/ltftUtilities";
 import { Button } from "@aws-amplify/ui-react";
 import { loadCctList } from "../../../redux/slices/cctListSlice";
 import { useSubmitting } from "../../../utilities/hooks/useSubmitting";
+import { ActionModal } from "../../common/ActionModal";
 
 const columnHelper = createColumnHelper<CctCalculation>();
 
@@ -72,12 +73,19 @@ const columnsDefault = [
 ];
 
 const createColumns = (
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsCctModalOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  setCalcToDelete: React.Dispatch<React.SetStateAction<string | null>>
 ) => {
   const ltftColumn = columnHelper.display({
     id: "makeLtft",
     cell: props => (
-      <RowCctActions row={props.row.original} setIsModalOpen={setIsModalOpen} />
+      <RowCctActions
+        row={props.row.original}
+        setIsModalOpen={setIsModalOpen}
+        setIsCctModalOpen={setIsCctModalOpen}
+        setCalcToDelete={setCalcToDelete}
+      />
     )
   });
   return [...columnsDefault, ltftColumn];
@@ -99,10 +107,25 @@ export function CctSavedDraftsTable() {
   ]);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCctModalOpen, setIsCctModalOpen] = useState(false);
+  const [calcIdToDelete, setCalcIdToDelete] = useState<string | null>(null);
+  const { startSubmitting, stopSubmitting } = useSubmitting();
+
+  const deleteCctCalcAndReloadList = async () => {
+    setIsCctModalOpen(false);
+    startSubmitting();
+    await store.dispatch(deleteCctCalc(calcIdToDelete as string));
+    const deleteStatus = store.getState().cct.formDeleteStatus;
+    if (deleteStatus === "succeeded") {
+      store.dispatch(loadCctList());
+    }
+    stopSubmitting();
+    setCalcIdToDelete(null);
+  };
 
   const columns = useMemo(
-    () => createColumns(setIsModalOpen),
-    [setIsModalOpen]
+    () => createColumns(setIsModalOpen, setIsCctModalOpen, setCalcIdToDelete),
+    [setIsCctModalOpen, setIsModalOpen, setCalcIdToDelete]
   );
 
   const table = useReactTable({
@@ -177,6 +200,16 @@ export function CctSavedDraftsTable() {
           history.push("/ltft/create");
         }}
       />
+      <ActionModal
+        onSubmit={deleteCctCalcAndReloadList}
+        isOpen={isCctModalOpen}
+        onClose={() => setIsCctModalOpen(false)}
+        cancelBtnText="Cancel"
+        warningLabel="Delete calculation"
+        warningText="Are you sure you want to delete this calculation? This action cannot be undone."
+        submittingBtnText="Deleting..."
+        actionType="Delete"
+      />
     </div>
   ) : (
     <p data-cy="no-saved-drafts">You have no saved calculations.</p>
@@ -186,14 +219,18 @@ export function CctSavedDraftsTable() {
 type RowLtftActionsProps = {
   row: CctCalculation;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsCctModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setCalcToDelete: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
 export function RowCctActions({
   row,
-  setIsModalOpen
+  setIsModalOpen,
+  setIsCctModalOpen,
+  setCalcToDelete
 }: Readonly<RowLtftActionsProps>) {
   const isLtftPilot = useIsLtftPilot();
-  const { isSubmitting, startSubmitting, stopSubmitting } = useSubmitting();
+  const { isSubmitting } = useSubmitting();
 
   const makeLtftBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
@@ -201,49 +238,38 @@ export function RowCctActions({
     setIsModalOpen(true);
   };
 
-  const deleteCctCalcAndReloadList = async (
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
+  const makeCctBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
-    startSubmitting();
-    await store.dispatch(deleteCctCalc(row.id as string));
-    const deleteStatus = store.getState().cct.formDeleteStatus;
-    if (deleteStatus === "succeeded") {
-      store.dispatch(loadCctList());
-    }
-    stopSubmitting();
+    setCalcToDelete(row.id as string);
+    setIsCctModalOpen(true);
   };
 
   return (
     <div style={{ display: "flex", gap: "1em" }}>
       {isLtftPilot && (
         <Button
+          type="button"
+          variation="primary"
+          size="small"
           onClick={makeLtftBtnClick}
           data-cy={`make-ltft-btn-${row.id}`}
-          size="small"
-          type="button"
           style={{
             minWidth: "18em",
-            maxWidth: "18em",
-            backgroundColor: "#005eb8",
-            color: "white",
-            cursor: "pointer"
+            maxWidth: "18em"
           }}
         >
           Apply for Changing hours (LTFT)
         </Button>
       )}
       <Button
-        onClick={deleteCctCalcAndReloadList}
-        data-cy={`delete-cct-btn-${row.id}`}
-        size="small"
         type="button"
-        style={{
-          backgroundColor: "#d5281b",
-          color: "white",
-          cursor: "pointer"
-        }}
-        disabled={isSubmitting}
+        variation="destructive"
+        size="small"
+        isDisabled={isSubmitting}
+        isLoading={isSubmitting}
+        loadingText="Deleting..."
+        onClick={makeCctBtnClick}
+        data-cy={`delete-cct-btn-${row.id}`}
       >
         Delete
       </Button>
