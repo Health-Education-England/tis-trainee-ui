1de232fff6c2e29e00c11b2b690f14258b1ea5ed
EdwardBarclay <Edward.Barclay@hee.nhs.uk>
2025-06-03 10:04:01 +0100

    refactor: remove mentions of RITA from form r

--- utilities/Constants.tsx
+++ utilities/Constants.tsx
@@ -134,7 +134,7 @@ export const FORMR_PARTB_ACCEPTANCE =
   "This form is a true and accurate declaration at this point in time and will immediately notify the Deanery/HEE local team and my employer if I am aware of any changes to the information provided in this form.";
 
 export const FORMR_PARTB_CONSENT =
-  "I give permission for my past and present ARCP/RITA portfolios and / or appraisal documentation to be viewed by my Responsible Officer and any appropriate person nominated by the Responsible Officer. Additionally if my Responsible Officer or Designated Body changes during my training period, I give permission for my current Responsible Officer to share this information with my new Responsible Officer for the purposes of Revalidation.";
+  "I give permission for my past and present ARCP portfolios and / or appraisal documentation to be viewed by my Responsible Officer and any appropriate person nominated by the Responsible Officer. Additionally if my Responsible Officer or Designated Body changes during my training period, I give permission for my current Responsible Officer to share this information with my new Responsible Officer for the purposes of Revalidation.";
 
 export const COVID_RESULT_DECLARATIONS = [
   {
