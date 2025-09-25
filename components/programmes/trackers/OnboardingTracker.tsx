4f5ad525780a48f07fc2b34eb768a514426892d4
john12321 <johnomeara72@gmail.com>
2025-09-25 10:05:38 +0100

    feat: add action statuses to PM onboarding tracker
    1. OnboardingTracker.tsx
    
    Action statuses added for:
    REVIEW_DATA (PM & P)
    SIGN_COJ
    SIGN_FORM_R_PART_A
    SIGN_FORM_R_PART_B
    TRAINING_NUMBER
    
    The above have a status of either “outstanding” or “completed”.
    The absence of an action from list of outstanding actions will deem it “completed”.
    The TRAINING_NUMBER status is “outstanding” if any of COJ or FORM_R actions are “outstanding”.
    Tracker section actions are "not available" when they are outside the required 'trigger date' e.g. 12 weeks actions will show "not available" if at 16 weeks etc.
    
    TODO: Look to track emails and other actions.

--- components/programmes/trackers/OnboardingTracker.tsx
+++ components/programmes/trackers/OnboardingTracker.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import React from "react";
 import { BackLink, Fieldset } from "nhsuk-react-components";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../redux/hooks/hooks";
-import { OnboardingTrackerType } from "../../../models/Tracker";
-import ErrorPage from "../../common/ErrorPage";
-import { mockOnboardingTrackerStatus1 } from "../../../mock-data/mockOnboardingTrackerStatus";
 import { OnboardingTrackerActions } from "./OnboardingTrackerActions";
-import Loading from "../../common/Loading";
-import dayjs from "dayjs";
 import history from "../../navigation/history";
 import ScrollToTop from "../../common/ScrollToTop";
+import { ProgrammeMembership } from "../../../models/ProgrammeMembership";
+import ErrorPage from "../../common/ErrorPage";
 
 export function OnboardingTracker() {
-  const [trackerData, setTrackerData] = useState<OnboardingTrackerType>(
-    mockOnboardingTrackerStatus1
-  );
-  const [loading, setLoading] = useState<boolean>(false); // set to true when API is ready
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const { id } = useParams<{ id: string }>();
 
   const panel = useAppSelector(state =>
@@ -26,22 +17,7 @@ export function OnboardingTracker() {
     )
   );
 
-  // TODO: Have a useEffect to fetch the tracker data when status/API work is done, then uncomment the rest of the code.
-
-  // if (loading) {
-  //   return <Loading />;
-  // }
-
-  // if (errorMsg) {
-  //   return (
-  //     <ErrorPage
-  //       header="Oops! There was a problem fetching your Tracker data."
-  //       message={`Please check your internet connection and click the NHS logo to reload the page. If the problem persists, please contact Support with a screenshot of this error if possible. (Error: ${errorMsg})`}
-  //     />
-  //   );
-  // }
-
-  return (
+  return panel ? (
     <>
       <ScrollToTop errors={[]} page={0} isPageDirty={false} />
       <BackLink
@@ -59,20 +35,12 @@ export function OnboardingTracker() {
           tabIndex={0}
           style={{ color: "#005eb8" }}
         >
-          {`The 'New Programme' onboarding journey`}
+          {`Onboarding Tracker for ${panel?.programmeName}`}
         </Fieldset.Legend>
-        {/* TODO Show the linkage when status/API work is done */}
-        {/* <h2 tabIndex={0} style={{ marginBottom: 0 }}>
-          {panel?.programmeName}
-        </h2>
-        <p tabIndex={0}>
-          <b>{`Starting ${dayjs(panel?.startDate).format("DD/MM/YYYY")}`}</b>
-        </p> */}
       </Fieldset>
-      <OnboardingTrackerActions
-        trackerStatusData={trackerData.trackerStatus}
-        progStartDate={panel?.startDate}
-      />
+      <OnboardingTrackerActions panel={panel as ProgrammeMembership} />
     </>
+  ) : (
+    <ErrorPage message="No Tracker data found for this programme. Please try again." />
   );
 }
