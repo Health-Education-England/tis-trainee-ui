cd4c28680520bc3e6b181b567f978b0c73d7f77f
DorisWongNHS <doris.wong@hee.nhs.uk>
2025-10-06 15:17:34 +0100

    fix: update notification to fit paged endpoint

--- redux/slices/notificationsSlice.ts
+++ redux/slices/notificationsSlice.ts
@@ -10,6 +10,18 @@ export type NotificationStatus =
   | "SENT"
   | "ARCHIVED";
 
+export type NotificationPage = {
+  content: NotificationType[];
+  page: PageDetails;
+};
+
+export type PageDetails = {
+  size: number;
+  number: number;
+  totalElements: number;
+  totalPages:number;
+};
+
 export type NotificationMsgType = "IN_APP";
 
 export type NotificationType = {
@@ -51,10 +63,10 @@ export const initialState: NotificationsState = {
 
 export const getNotifications = createAsyncThunk(
   "notifications/getNotifications",
-  async () => {
+  async (params?: Record<string, string>) => {
     const notificationService = new TraineeNotificationsService();
-    const response = await notificationService.getAllNotifications();
-    return response.data.filter(n => n.type == "IN_APP");
+    const response = await notificationService.getAllNotifications(params);
+    return response.data;
   }
 );
 
@@ -126,7 +138,7 @@ const notificationsSlice = createSlice({
       })
       .addCase(getNotifications.fulfilled, (state, action) => {
         state.status = "succeeded";
-        state.notificationsList = action.payload;
+        state.notificationsList = action.payload.content;
         state.unreadNotificationCount = unreadNotificationsCount(
           action.payload ?? 0
         );
@@ -215,9 +227,10 @@ export const {
   updatedNotificationUpdateInProgress
 } = notificationsSlice.actions;
 
-export function unreadNotificationsCount(notificationsData: any[]) {
-  if (!Array.isArray(notificationsData)) return 0;
-  const unreadNotifications = notificationsData.filter(
+export function unreadNotificationsCount(notificationsData: NotificationPage) {
+  let notifications = notificationsData.content;
+  if (!Array.isArray(notifications)) return 0;
+  const unreadNotifications = notifications.filter(
     notification => notification.status === "UNREAD"
   );
   return unreadNotifications.length;
--- services/TraineeNotificationsService.ts
+++ services/TraineeNotificationsService.ts
@@ -1,13 +1,21 @@
 import { AxiosResponse } from "axios";
 import ApiService from "./apiService";
+import { NotificationPage } from "../redux/slices/notificationsSlice";
 
 export class TraineeNotificationsService extends ApiService {
   constructor() {
     super("/api");
   }
-
-  async getAllNotifications(): Promise<AxiosResponse<any[]>> {
-    return this.get<any[]>(`/notifications`);
+  async getAllNotifications(
+    params?: Record<string, string | number>
+  ): Promise<AxiosResponse<NotificationPage>> {    
+    const page = params?.page ? params?.page : "";
+    const size = params?.size ? params?.size : "";
+    const sort = params?.sort ? params?.sort : "";
+    const type = params?.type ? params?.type : "";
+    const status = params?.status ? params?.status : "";
+    const keyword = params?.keyword ? params?.keyword : "";
+    return this.get<NotificationPage>(`/notifications?page=${page}&size=${size}&sort=${sort}&type=${type}&status=${status}&keyword=${keyword}`);
   }
 
   async markNotificationAsRead(
