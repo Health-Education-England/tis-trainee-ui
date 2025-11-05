b4a3333b485003cd68ce2e154b874eec143483eb
john12321 <johnomeara72@gmail.com>
2025-11-05 00:05:51 +0000

    test: updated action and notification logic

--- components/notifications/__test__/RowActions.test.tsx
+++ components/notifications/__test__/RowActions.test.tsx
@@ -1,19 +1,31 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import { RowActions } from "../RowActions";
-import { notificationsData } from "../../../mock-data/notifications";
+import { mockInAppNotificationsData } from "../../../mock-data/mock-notifications-data";
 import store from "../../../redux/store/store";
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import history from "../../../components/navigation/history";
-import { updateNotificationStatus } from "../../../utilities/NotificationsUtilities";
+import {
+  markAsUnreadBE,
+  markAsUnreadFE
+} from "../../../utilities/NotificationsUtilities";
 
 jest.mock("../../../utilities/NotificationsUtilities", () => ({
-  ...jest.requireActual("../../../utilities/NotificationsUtilities"),
-  updateNotificationStatus: jest.fn()
+  markAsUnreadBE: jest.fn(),
+  markAsUnreadFE: jest.fn()
 }));
 
-test("RowActions", () => {
-  const row = notificationsData[1];
+const mockMarkAsUnreadBE = markAsUnreadBE as jest.MockedFunction<
+  typeof markAsUnreadBE
+>;
+const mockMarkAsUnreadFE = markAsUnreadFE as jest.MockedFunction<
+  typeof markAsUnreadFE
+>;
+
+test("RowActions", async () => {
+  mockMarkAsUnreadBE.mockResolvedValue(undefined);
+
+  const row = mockInAppNotificationsData[3];
   const { getByRole } = render(
     <Provider store={store}>
       <Router history={history}>
@@ -22,6 +34,17 @@ test("RowActions", () => {
     </Provider>
   );
   const button = getByRole("button", { name: /Mark as unread/i });
+
   fireEvent.click(button);
-  expect(updateNotificationStatus).toHaveBeenCalledWith(row, "UNREAD");
+
+  expect(mockMarkAsUnreadFE).toHaveBeenCalledWith(row);
+  expect(button).toBeDisabled();
+
+  await waitFor(() => {
+    expect(mockMarkAsUnreadBE).toHaveBeenCalledWith(row);
+  });
+
+  await waitFor(() => {
+    expect(button).not.toBeDisabled();
+  });
 });
