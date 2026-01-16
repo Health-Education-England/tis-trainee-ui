3e2dd01dd51fcf013f58036cb34e27bccb8ad297
EdwardBarclay <Edward.Barclay@hee.nhs.uk>
2026-01-16 13:55:02 +0000

    test: remove orphaned check

--- cypress/e2e/home/Home.spec.ts
+++ cypress/e2e/home/Home.spec.ts
@@ -16,9 +16,6 @@ describe("Home", () => {
   });
 
   it("should find profile button and click in to the profile section", () => {
-    cy.get(".nhsuk-action-link__link")
-      .should("have.attr", "href")
-      .and("contain", "forms.office.com/pages/responsepage.aspx");
     cy.get('[data-cy="homeLink"]').should("not.exist");
     cy.get('[data-cy="homeWelcomeHeaderText"]')
       .should("exist")
