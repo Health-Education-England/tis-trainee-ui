e644184cc529fab3a81118ab3ea1bff204d02784
Reuben Roberts <reuben.roberts@nhs.net>
2026-03-16 16:14:49 +0000

    feat: recruit deferrral banner
    Design as per previous survey banner.
    
    TIS21-8192

--- components/main/__test__/GlobalAlert.test.tsx
+++ components/main/__test__/GlobalAlert.test.tsx
@@ -125,6 +125,24 @@ describe("GlobalAlert", () => {
     expect(screen.getByText(/We have moved/i)).toBeInTheDocument();
   });
 
+  test("renders recruit alert by default and can dismiss it", async () => {
+    const { container } = renderWithProviders(<GlobalAlert />);
+
+    const recruitAlert = queryByAttribute("data-cy", container, "recruitAlert");
+    expect(recruitAlert).toBeInTheDocument();
+    expect(screen.getByText(/deferral/i)).toBeInTheDocument();
+
+    const dismissButton = recruitAlert?.querySelector("button");
+    expect(dismissButton).toBeInTheDocument();
+    dismissButton && dismissButton.click();
+
+    renderWithProviders(<GlobalAlert />);
+
+    expect(
+      queryByAttribute("data-cy", container, "recruitAlert")
+    ).not.toBeInTheDocument();
+  });
+
   test("renders all alerts when conditions for them are met", () => {
     mockUseTraineeActions.mockReturnValue({ hasOutstandingActions: true });
 
@@ -148,6 +166,9 @@ describe("GlobalAlert", () => {
     ).toBeInTheDocument();
     expect(
       queryByAttribute("data-cy", container, "bookmarkAlert")
+    ).toBeInTheDocument();
+    expect(
+      queryByAttribute("data-cy", container, "recruitAlert")
     ).toBeInTheDocument();
   });
 
