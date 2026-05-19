b2f37fa6de17195cbddecfb8b8d519e871eec973
john12321 <johnomeara72@gmail.com>
2026-05-19 17:05:27 +0100

    fix: remove PM CCT e2e tests

--- cypress/e2e/programmes/Programmes.spec.ts
+++ cypress/e2e/programmes/Programmes.spec.ts
@@ -21,37 +21,6 @@ describe("Programmes", () => {
     cy.get(".nhsuk-fieldset__heading").contains(
       "Certificate of Completion of Training (CCT)"
     );
-    cy.get('[data-cy="cct-home-new-calc-btn"]').click();
-    cy.checkAndFillNewCctCalcForm();
-
-    // edit new wte
-    cy.get('[data-cy="cct-view-new-wte"]').contains("90%");
-    cy.get('[data-cy="cct-edit-btn"]').click();
-    cy.clickSelect('[data-cy="changes[0].wte"]', "60%");
-    cy.get('[data-cy="cct-calc-btn"]').click();
-    cy.get('[data-cy="cct-view-new-wte"]').contains("60%");
-
-    // name and save new calc
-    cy.get('[data-cy="cct-save-btn"]').should("exist").click();
-    cy.get('[data-cy="name"]').type(`😎_${dayjs().toISOString()}`, {
-      parseSpecialCharSequences: false
-    });
-    cy.get('[data-cy="cct-modal-save-btn"]').click();
-    cy.get('[data-cy="cct-home-subheader-calcs"]').contains(
-      "Saved calculations"
-    );
-    cy.get('[data-cy*="_name"]')
-      .first()
-      .should("include.text", `😎_${dayjs().format("YYYY-MM-DD")}`);
-    cy.get('[data-cy*="_name"]').first().click();
-    cy.get('[data-cy="saved-cct-details"] > :nth-child(1)').should(
-      "include.text",
-      `Name: 😎_${dayjs().format("YYYY-MM-DD")}`
-    );
-    cy.get('[data-cy="cct-save-btn"]').should("not.exist");
-    // back to cct home via nav link
-    cy.get('[data-cy="CCT"]').click();
-    cy.get('[data-cy="cct-home-subheader-calcs"]').should("exist");
   });
 });
 
