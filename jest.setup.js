bc516bae3ea98d05eb318ccfa1e52bb40b9a0a00
john12321 <johnomeara72@gmail.com>
2026-06-25 15:59:36 +0100

    fix(Jest tests): mock NHS UK Button in Jest
    - prevent NHS frontend dynamic imports from breaking Jest
    - keep remaining NHS components using their real implementations
    
    Note: did try an experimental VM modules workaround first but hings got messy as we'd have to enable experimental ESM behaviour across the entire Jest test suite.

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,41 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
+
+jest.mock("nhsuk-react-components", () => {
+  const React = require("react");
+  const actual = jest.requireActual("nhsuk-react-components");
+
+  const Button = React.forwardRef(
+    (
+      {
+        as,
+        asElement,
+        className,
+        login,
+        preventDoubleClick,
+        reverse,
+        secondary,
+        secondarySolid,
+        small,
+        warning,
+        ...props
+      },
+      ref
+    ) => {
+      const Element = asElement ?? (as === "a" ? "a" : "button");
+
+      return React.createElement(Element, {
+        ...props,
+        className,
+        ref
+      });
+    }
+  );
+
+  Button.displayName = "MockNhsukButton";
+
+  return {
+    ...actual,
+    Button
+  };
+});
