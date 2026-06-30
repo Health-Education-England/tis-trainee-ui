52c658e7b26d297368d549766899c5022f06008c
pete webb <peter.webb11@nhs.net>
2026-06-30 11:31:34 +0100

    refactor: general footer refactored to mix of nhs component and semantic html

--- components/navigation/TSSFooter.tsx
+++ components/navigation/TSSFooter.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "nhsuk-react-components";
 import { NavLink } from "react-router-dom";
+import styles from "./TSSFooter.module.scss";
 
 interface TSSFooterProps {
   appVersion: string;
@@ -7,42 +8,45 @@ interface TSSFooterProps {
 
 const TSSFooter = ({ appVersion }: TSSFooterProps) => {
   return (
-    <Footer>
-      <Footer.Meta>
-        <Footer.ListItem
-          asElement={NavLink}
-          data-cy="linkSupport"
-          href="/support"
-          to="/support"
-        >
-          Support
-        </Footer.ListItem>
-        <Footer.ListItem
-          data-cy="linkAbout"
-          href="https://tis-support.hee.nhs.uk/about-tis/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          About
-        </Footer.ListItem>
-        <Footer.ListItem
-          data-cy="linkPrivacyPolicy"
-          href="https://www.hee.nhs.uk/about/privacy-notice"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Privacy & Cookies Policy
-        </Footer.ListItem>
-        <Footer.Copyright data-cy="copyrightText">
-          &copy; NHS England
-          {appVersion && (
-            <span
-              className="nhsuk-u-display-block nhsuk-u-margin-top-2"
-              data-cy="versionText"
-            >{`version: ${appVersion}`}</span>
-          )}
-        </Footer.Copyright>
-      </Footer.Meta>
+    <Footer className={styles.footerWrapper}>
+      <Footer.Content width="full">
+        <nav className={styles.footerNav} aria-label="Footer navigation">
+          <ul className={styles.footerList}>
+            <li>
+              <NavLink to="/support" data-cy="linkSupport">
+                Support
+              </NavLink>
+            </li>
+
+            <li>
+              <a
+                href="https://tis-support.hee.nhs.uk/about-tis/"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-cy="linkAbout"
+              >
+                About
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://www.hee.nhs.uk/about/privacy-notice"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-cy="linkPrivacyPolicy"
+              >
+                Privacy & Cookies Policy
+              </a>
+            </li>
+            {appVersion && (
+              <li className={styles.version} data-cy="versionText">
+                Version: {appVersion}
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Footer.Content>
     </Footer>
   );
 };
